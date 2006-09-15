2f46484204da80eee99bca19c5fb864bd8692c64
adjemian <adjemian@ac1d8469-bf42-47a9-8791-bf33cf982152>
2006-09-15 14:25:41 +0000

    partially known steady state
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@915 ac1d8469-bf42-47a9-8791-bf33cf982152

---
+++ matlab/restricted_steadystate.m
@@ -0,0 +1,20 @@
+function [sR,sG] = restricted_steadystate(y,x,indx)
+  % stephane.adjemian@gmail.com
+  global options_ M_ oo_
+  
+  inde  = options_.steadystate_partial.sseqn;
+  
+  ss = oo_.steady_state;
+  
+  ss(indx) = y;
+ 
+  eval(['[R,G] = ' M_.fname '_static(ss,x);']);
+
+  sR = R(inde);
+  sG = G(inde,indx);
+  
+  sG
+ 
+  null(sG)
+  
+  pause
