dabe982836e8d397ea231da9416790072dd02443
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2006-09-18 08:02:45 +0000

    v4: new version of include/DynareBison.h
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@919 ac1d8469-bf42-47a9-8791-bf33cf982152

--- parser.src/include/DynareBison.h
+++ parser.src/include/DynareBison.h
@@ -75,108 +75,110 @@
      IRF = 301,
      KALMAN_ALGO = 302,
      KALMAN_TOL = 303,
-     LAPLACE = 304,
-     LIK_ALGO = 305,
-     LIK_INIT = 306,
-     LINEAR = 307,
-     LOAD_MH_FILE = 308,
-     LOGLINEAR = 309,
-     MH_DROP = 310,
-     MH_INIT_SCALE = 311,
-     MH_JSCALE = 312,
-     MH_MODE = 313,
-     MH_NBLOCKS = 314,
-     MH_REPLIC = 315,
-     MODE_CHECK = 316,
-     MODE_COMPUTE = 317,
-     MODE_FILE = 318,
-     MODEL = 319,
-     MODEL_COMPARISON = 320,
-     MODEL_COMPARISON_APPROXIMATION = 321,
-     MODIFIEDHARMONICMEAN = 322,
-     MOMENTS = 323,
-     MOMENTS_VARENDO = 324,
-     MSHOCKS = 325,
-     NAME = 326,
-     NOBS = 327,
-     NOCORR = 328,
-     NODIAGNOSTIC = 329,
-     NOFUNCTIONS = 330,
-     NOGRAPH = 331,
-     XLS_SHEET = 332,
-     XLS_RANGE = 333,
-     NOMOMENTS = 334,
-     NOPRINT = 335,
-     NORMAL_PDF = 336,
-     OBSERVATION_TRENDS = 337,
-     OLR = 338,
-     OLR_INST = 339,
-     OLR_BETA = 340,
-     OPTIM = 341,
-     OPTIM_WEIGHTS = 342,
-     ORDER = 343,
-     OSR = 344,
-     OSR_PARAMS = 345,
-     PARAMETERS = 346,
-     PERIODS = 347,
-     PREFILTER = 348,
-     PRESAMPLE = 349,
-     PRINT = 350,
-     PRIOR_TRUNC = 351,
-     FILTER_STEP_AHEAD = 352,
-     QZ_CRITERIUM = 353,
-     RELATIVE_IRF = 354,
-     REPLIC = 355,
-     RESOL = 356,
-     RPLOT = 357,
-     SHOCKS = 358,
-     SIGMA_E = 359,
-     SIMUL = 360,
-     SIMUL_ALGO = 361,
-     SIMUL_SEED = 362,
-     SMOOTHER = 363,
-     SOLVE_ALGO = 364,
-     STDERR = 365,
-     STEADY = 366,
-     STOCH_SIMUL = 367,
-     TEX = 368,
-     TEX_NAME = 369,
-     UNIFORM_PDF = 370,
-     UNIT_ROOT_VARS = 371,
-     USE_DLL = 372,
-     VALUES = 373,
-     VAR = 374,
-     VAREXO = 375,
-     VAREXO_DET = 376,
-     VAROBS = 377,
-     XTICK = 378,
-     XTICKLABEL = 379,
-     COMMA = 380,
-     MINUS = 381,
-     PLUS = 382,
-     DIVIDE = 383,
-     TIMES = 384,
-     UMINUS = 385,
-     POWER = 386,
-     FACTORIAL = 387,
-     EXP = 388,
-     LOG = 389,
-     LOG10 = 390,
-     LN = 391,
-     SIN = 392,
-     COS = 393,
-     TAN = 394,
-     ASIN = 395,
-     ACOS = 396,
-     ATAN = 397,
-     SINH = 398,
-     COSH = 399,
-     TANH = 400,
-     ASINH = 401,
-     ACOSH = 402,
-     ATANH = 403,
-     SQRT = 404,
-     ASSIGN = 405
+     CONSTANT = 304,
+     NOCONSTANT = 305,
+     LAPLACE = 306,
+     LIK_ALGO = 307,
+     LIK_INIT = 308,
+     LINEAR = 309,
+     LOAD_MH_FILE = 310,
+     LOGLINEAR = 311,
+     MH_DROP = 312,
+     MH_INIT_SCALE = 313,
+     MH_JSCALE = 314,
+     MH_MODE = 315,
+     MH_NBLOCKS = 316,
+     MH_REPLIC = 317,
+     MODE_CHECK = 318,
+     MODE_COMPUTE = 319,
+     MODE_FILE = 320,
+     MODEL = 321,
+     MODEL_COMPARISON = 322,
+     MODEL_COMPARISON_APPROXIMATION = 323,
+     MODIFIEDHARMONICMEAN = 324,
+     MOMENTS = 325,
+     MOMENTS_VARENDO = 326,
+     MSHOCKS = 327,
+     NAME = 328,
+     NOBS = 329,
+     NOCORR = 330,
+     NODIAGNOSTIC = 331,
+     NOFUNCTIONS = 332,
+     NOGRAPH = 333,
+     XLS_SHEET = 334,
+     XLS_RANGE = 335,
+     NOMOMENTS = 336,
+     NOPRINT = 337,
+     NORMAL_PDF = 338,
+     OBSERVATION_TRENDS = 339,
+     OLR = 340,
+     OLR_INST = 341,
+     OLR_BETA = 342,
+     OPTIM = 343,
+     OPTIM_WEIGHTS = 344,
+     ORDER = 345,
+     OSR = 346,
+     OSR_PARAMS = 347,
+     PARAMETERS = 348,
+     PERIODS = 349,
+     PREFILTER = 350,
+     PRESAMPLE = 351,
+     PRINT = 352,
+     PRIOR_TRUNC = 353,
+     FILTER_STEP_AHEAD = 354,
+     QZ_CRITERIUM = 355,
+     RELATIVE_IRF = 356,
+     REPLIC = 357,
+     RESOL = 358,
+     RPLOT = 359,
+     SHOCKS = 360,
+     SIGMA_E = 361,
+     SIMUL = 362,
+     SIMUL_ALGO = 363,
+     SIMUL_SEED = 364,
+     SMOOTHER = 365,
+     SOLVE_ALGO = 366,
+     STDERR = 367,
+     STEADY = 368,
+     STOCH_SIMUL = 369,
+     TEX = 370,
+     TEX_NAME = 371,
+     UNIFORM_PDF = 372,
+     UNIT_ROOT_VARS = 373,
+     USE_DLL = 374,
+     VALUES = 375,
+     VAR = 376,
+     VAREXO = 377,
+     VAREXO_DET = 378,
+     VAROBS = 379,
+     XTICK = 380,
+     XTICKLABEL = 381,
+     COMMA = 382,
+     MINUS = 383,
+     PLUS = 384,
+     DIVIDE = 385,
+     TIMES = 386,
+     UMINUS = 387,
+     POWER = 388,
+     FACTORIAL = 389,
+     EXP = 390,
+     LOG = 391,
+     LOG10 = 392,
+     LN = 393,
+     SIN = 394,
+     COS = 395,
+     TAN = 396,
+     ASIN = 397,
+     ACOS = 398,
+     ATAN = 399,
+     SINH = 400,
+     COSH = 401,
+     TANH = 402,
+     ASINH = 403,
+     ACOSH = 404,
+     ATANH = 405,
+     SQRT = 406,
+     ASSIGN = 407
    };
 #endif
 /* Tokens.  */
@@ -226,108 +228,110 @@
 #define IRF 301
 #define KALMAN_ALGO 302
 #define KALMAN_TOL 303
-#define LAPLACE 304
-#define LIK_ALGO 305
-#define LIK_INIT 306
-#define LINEAR 307
-#define LOAD_MH_FILE 308
-#define LOGLINEAR 309
-#define MH_DROP 310
-#define MH_INIT_SCALE 311
-#define MH_JSCALE 312
-#define MH_MODE 313
-#define MH_NBLOCKS 314
-#define MH_REPLIC 315
-#define MODE_CHECK 316
-#define MODE_COMPUTE 317
-#define MODE_FILE 318
-#define MODEL 319
-#define MODEL_COMPARISON 320
-#define MODEL_COMPARISON_APPROXIMATION 321
-#define MODIFIEDHARMONICMEAN 322
-#define MOMENTS 323
-#define MOMENTS_VARENDO 324
-#define MSHOCKS 325
-#define NAME 326
-#define NOBS 327
-#define NOCORR 328
-#define NODIAGNOSTIC 329
-#define NOFUNCTIONS 330
-#define NOGRAPH 331
-#define XLS_SHEET 332
-#define XLS_RANGE 333
-#define NOMOMENTS 334
-#define NOPRINT 335
-#define NORMAL_PDF 336
-#define OBSERVATION_TRENDS 337
-#define OLR 338
-#define OLR_INST 339
-#define OLR_BETA 340
-#define OPTIM 341
-#define OPTIM_WEIGHTS 342
-#define ORDER 343
-#define OSR 344
-#define OSR_PARAMS 345
-#define PARAMETERS 346
-#define PERIODS 347
-#define PREFILTER 348
-#define PRESAMPLE 349
-#define PRINT 350
-#define PRIOR_TRUNC 351
-#define FILTER_STEP_AHEAD 352
-#define QZ_CRITERIUM 353
-#define RELATIVE_IRF 354
-#define REPLIC 355
-#define RESOL 356
-#define RPLOT 357
-#define SHOCKS 358
-#define SIGMA_E 359
-#define SIMUL 360
-#define SIMUL_ALGO 361
-#define SIMUL_SEED 362
-#define SMOOTHER 363
-#define SOLVE_ALGO 364
-#define STDERR 365
-#define STEADY 366
-#define STOCH_SIMUL 367
-#define TEX 368
-#define TEX_NAME 369
-#define UNIFORM_PDF 370
-#define UNIT_ROOT_VARS 371
-#define USE_DLL 372
-#define VALUES 373
-#define VAR 374
-#define VAREXO 375
-#define VAREXO_DET 376
-#define VAROBS 377
-#define XTICK 378
-#define XTICKLABEL 379
-#define COMMA 380
-#define MINUS 381
-#define PLUS 382
-#define DIVIDE 383
-#define TIMES 384
-#define UMINUS 385
-#define POWER 386
-#define FACTORIAL 387
-#define EXP 388
-#define LOG 389
-#define LOG10 390
-#define LN 391
-#define SIN 392
-#define COS 393
-#define TAN 394
-#define ASIN 395
-#define ACOS 396
-#define ATAN 397
-#define SINH 398
-#define COSH 399
-#define TANH 400
-#define ASINH 401
-#define ACOSH 402
-#define ATANH 403
-#define SQRT 404
-#define ASSIGN 405
+#define CONSTANT 304
+#define NOCONSTANT 305
+#define LAPLACE 306
+#define LIK_ALGO 307
+#define LIK_INIT 308
+#define LINEAR 309
+#define LOAD_MH_FILE 310
+#define LOGLINEAR 311
+#define MH_DROP 312
+#define MH_INIT_SCALE 313
+#define MH_JSCALE 314
+#define MH_MODE 315
+#define MH_NBLOCKS 316
+#define MH_REPLIC 317
+#define MODE_CHECK 318
+#define MODE_COMPUTE 319
+#define MODE_FILE 320
+#define MODEL 321
+#define MODEL_COMPARISON 322
+#define MODEL_COMPARISON_APPROXIMATION 323
+#define MODIFIEDHARMONICMEAN 324
+#define MOMENTS 325
+#define MOMENTS_VARENDO 326
+#define MSHOCKS 327
+#define NAME 328
+#define NOBS 329
+#define NOCORR 330
+#define NODIAGNOSTIC 331
+#define NOFUNCTIONS 332
+#define NOGRAPH 333
+#define XLS_SHEET 334
+#define XLS_RANGE 335
+#define NOMOMENTS 336
+#define NOPRINT 337
+#define NORMAL_PDF 338
+#define OBSERVATION_TRENDS 339
+#define OLR 340
+#define OLR_INST 341
+#define OLR_BETA 342
+#define OPTIM 343
+#define OPTIM_WEIGHTS 344
+#define ORDER 345
+#define OSR 346
+#define OSR_PARAMS 347
+#define PARAMETERS 348
+#define PERIODS 349
+#define PREFILTER 350
+#define PRESAMPLE 351
+#define PRINT 352
+#define PRIOR_TRUNC 353
+#define FILTER_STEP_AHEAD 354
+#define QZ_CRITERIUM 355
+#define RELATIVE_IRF 356
+#define REPLIC 357
+#define RESOL 358
+#define RPLOT 359
+#define SHOCKS 360
+#define SIGMA_E 361
+#define SIMUL 362
+#define SIMUL_ALGO 363
+#define SIMUL_SEED 364
+#define SMOOTHER 365
+#define SOLVE_ALGO 366
+#define STDERR 367
+#define STEADY 368
+#define STOCH_SIMUL 369
+#define TEX 370
+#define TEX_NAME 371
+#define UNIFORM_PDF 372
+#define UNIT_ROOT_VARS 373
+#define USE_DLL 374
+#define VALUES 375
+#define VAR 376
+#define VAREXO 377
+#define VAREXO_DET 378
+#define VAROBS 379
+#define XTICK 380
+#define XTICKLABEL 381
+#define COMMA 382
+#define MINUS 383
+#define PLUS 384
+#define DIVIDE 385
+#define TIMES 386
+#define UMINUS 387
+#define POWER 388
+#define FACTORIAL 389
+#define EXP 390
+#define LOG 391
+#define LOG10 392
+#define LN 393
+#define SIN 394
+#define COS 395
+#define TAN 396
+#define ASIN 397
+#define ACOS 398
+#define ATAN 399
+#define SINH 400
+#define COSH 401
+#define TANH 402
+#define ASINH 403
+#define ACOSH 404
+#define ATANH 405
+#define SQRT 406
+#define ASSIGN 407
 
 
 
