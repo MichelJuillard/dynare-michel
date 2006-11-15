c2a568cb2283e5a8a1444b216dd5eced66751a39
ratto <ratto@ac1d8469-bf42-47a9-8791-bf33cf982152>
2006-11-15 08:21:34 +0000

    Updates and management of memory.
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1073 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/dynare_MC.m
+++ matlab/dynare_MC.m
@@ -1,7 +1,10 @@
-function dynare_MC(var_list_)
+function dynare_MC(var_list_,OutDir)
 
 global M_ options_ oo_ estim_params_ 
 global bayestopt_
+
+% temporary fix until M_.H is initialized by the parser
+M_.H = [];
 
 options_.varlist = var_list_;
 options_.lgyidx2varobs = zeros(size(M_.endo_names,1),1);
@@ -38,7 +41,7 @@ options_ = set_default_option(options_,'XTickLabel',[]);
 options_ = set_default_option(options_,'bayesian_irf',0);
 options_ = set_default_option(options_,'bayesian_th_moments',0);
 options_ = set_default_option(options_,'TeX',0);
-options_ = set_default_option(options_,'irf',0);
+options_ = set_default_option(options_,'irf',40);
 options_ = set_default_option(options_,'relative_irf',0);
 options_ = set_default_option(options_,'order',1);
 options_ = set_default_option(options_,'ar',5);
@@ -57,9 +60,28 @@ options_ = set_default_option(options_,'posterior_mode_estimation',1);
 options_ = set_default_option(options_,'MaxNumberOfBytes',1e6);
 options_ = set_default_option(options_,'xls_sheet','');
 options_ = set_default_option(options_,'xls_range','');
+options_ = set_default_option(options_,'filter_step_ahead',0);
+options_ = set_default_option(options_,'diffuse_d',[]);
+options_ = set_default_option(options_,'Opt6Iter',3);
+options_ = set_default_option(options_,'Opt6Numb',100000);
 options_ = set_default_option(options_,'steadystate_flag',0);
+options_ = set_default_option(options_,'logdata',0);
+options_ = set_default_option(options_,'use_mh_covariance_matrix',0);
+
 if exist([M_.fname '_steadystate.m'])
   options_.steadystate_flag = 1;
+end
+
+if options_.filtered_vars ~= 0 & options_.filter_step_ahead == 0
+  options_.filter_step_ahead = 1;
+end
+if options_.bayesian_irf  & options_.irf == 0
+  options_.irf = 40;
+end
+if options_.filter_step_ahead ~= 0
+  options_.nk = max(options_.filter_step_ahead);
+else
+  options_.nk = 0;
 end
 
 %% Add something to the parser ++>
@@ -71,8 +93,6 @@ pnames 		= ['     ';'beta ';'gamm ';'norm ';'invg ';'unif ';'invg2'];
 n_varobs 	= size(options_.varobs,1);
 
 [xparam1,estim_params_,bayestopt_,lb,ub] = set_prior(estim_params_);
-
-options_.mh_replic = 0;
 
 bounds = prior_bounds(bayestopt_);
 bounds(:,1)=max(bounds(:,1),lb);
@@ -175,18 +195,29 @@ rawdata = read_variables(options_.datafile,options_.varobs,[],options_.xls_sheet
 k = [];
 k1 = [];
 for i=1:n_varobs
-  k = [k strmatch(deblank(options_.varobs(i,:)),M_.endo_names(dr.order_var,:), ...
-		  'exact')];
+  k = [k strmatch(deblank(options_.varobs(i,:)),M_.endo_names(dr.order_var,:),'exact')];
   k1 = [k1 strmatch(deblank(options_.varobs(i,:)),M_.endo_names, 'exact')];
 end
+% union of observed and state variables
+k2 = union(k',[dr.nstatic+1:dr.nstatic+dr.npred]');
+% including variables in t-2 and earlier, if any
+k2 = [k2;[M_.endo_nbr+(1:dr.nspred-dr.npred)]'];
 
-bayestopt_.mf 	= k;
+% set restrict_state to postion of observed + state variables
+% in expanded state vector
+bayestopt_.restrict_state = k2;
+% set mf1 to positions of observed variables in restricted state vector
+% for likelihood computation
+[junk,bayestopt_.mf1] = ismember(k,k2); 
+% set mf2 to positions of observed variables in expanded state vector
+% for filtering and smoothing
+bayestopt_.mf2 	= k;
 bayestopt_.mfys = k1;
 options_ = set_default_option(options_,'nobs',size(rawdata,1)-options_.first_obs+1);
 gend = options_.nobs;
 
 rawdata = rawdata(options_.first_obs:options_.first_obs+gend-1,:);
-if options_.loglinear == 1
+if options_.loglinear == 1 & ~options_.logdata
   rawdata = log(rawdata);
 end
 if options_.prefilter == 1
@@ -214,18 +245,17 @@ options_ = set_default_option(options_,'opt_gsa',1);
 options_gsa_ = options_.opt_gsa;
 
 if options_gsa_.pprior,
-  load([fname_,'_prior'])
   namfile=[fname_,'_prior'];
 else
-  load([fname_,'_mc'])
   namfile=[fname_,'_mc'];
 end
+load([OutDir,'\',namfile],'lpmat', 'lpmat0', 'istable')
 load(options_.mode_file)
 %%
 %%
 %%
 x=[lpmat0(istable,:) lpmat(istable,:)];
-clear lpmat lpmat0 istable iunstable egg yys T
+clear lpmat lpmat0 istable %iunstable egg yys T
 B = size(x,1);
 if M_.maximum_lag > 1
     l1 = flipud([cumsum(M_.lead_lag_incidence(1:M_.maximum_lag-1,dr.order_var),1);ones(1,M_.endo_nbr)]);
@@ -233,21 +263,47 @@ if M_.maximum_lag > 1
 else
     n1 = M_.endo_nbr;
 end
-stock_smooth = zeros(gend,n1,B);
-stock_filter = zeros(gend+1,n1,B);
-stock_ys = zeros(M_.endo_nbr,B);
+nfil=B/40;
+stock_smooth = zeros(gend,n1,40);
+stock_filter = zeros(gend+1,n1,40);
+stock_ys = zeros(M_.endo_nbr,40);
+logpo2=zeros(B,1);
 %%
 h = waitbar(0,'MC smoother ...');
+ib=0;
+ifil=0;
+opt_gsa=options_.opt_gsa;
 for b=1:B
+  ib=ib+1;
   deep = x(b,:);
   %deep(1:offset) = xparam1(1:offset);
-  logpo2(b) = DsgeLikelihood(deep',gend,data);
+  logpo2(b,1) = DsgeLikelihood(deep',gend,data);
+  if opt_gsa.lik_only==0,
   [atT,innov,measurement_error,filtered_state_vector,ys,trend_coeff] = DsgeSmoother(deep,gend,data);
-  stock_smooth(:,:,b)=atT';
-  stock_filter(:,:,b)=filtered_state_vector';
-  stock_ys(:,b)=ys;
+  stock_smooth(:,:,ib)=atT';
+  stock_filter(:,:,ib)=filtered_state_vector';
+  stock_ys(:,ib)=ys;
+  if ib==40,
+    ib=0;
+    ifil=ifil+1;
+    save([OutDir,'\',namfile,'_',num2str(ifil)],'stock_smooth','stock_filter','stock_ys')
+    stock_smooth = zeros(gend,n1,40);
+    stock_filter = zeros(gend+1,n1,40);
+    stock_ys = zeros(M_.endo_nbr,40);
+  end
+  end  
   waitbar(b/B,h,['MC smoother ...',num2str(b),'/',num2str(B)]);
 end
 close(h)
-
-save(namfile,'x','logpo2','stock_smooth','stock_filter','stock_ys','-append')
+if opt_gsa.lik_only==0,
+if ib>0,
+    ifil=ifil+1;
+    stock_smooth = stock_smooth(:,:,1:ib);
+    stock_filter = stock_filter(:,:,1:ib);
+    stock_ys = stock_ys(:,1:ib);
+    save([OutDir,'\',namfile,'_',num2str(ifil)],'stock_smooth','stock_filter','stock_ys')
+end
+end
+stock_gend=gend;
+stock_data=data;
+save([OutDir,'\',namfile],'x','logpo2','stock_gend','stock_data','-append')
