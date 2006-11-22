554c3c09edd5fd054244771c02db63cbfddb604a
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2006-11-22 08:20:49 +0000

    re-organisation of folders to package separately GSA tools
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@5 f1850c17-3b45-254b-b221-fcb05880fee1

--- matlab/name2cell.m
+++
@@ -1,8 +0,0 @@
-function name2cell(varargin);
-
-cname = varargin(2:end);
-if strcmp(varargin{1}(1),'-'),
-  assignin('caller',varargin{1}(2:end),cname)
-else
-  disp('The name of the cell variable MUST start with - (minus)!!!')
-end
