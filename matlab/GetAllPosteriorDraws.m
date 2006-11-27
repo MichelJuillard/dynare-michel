c37932d6f843f14c9ea4b654377a64a057307dd6
ratto <ratto@ac1d8469-bf42-47a9-8791-bf33cf982152>
2006-11-27 14:24:29 +0000

    Shifted order of loops for nblock and files, to easily allow cumulated mean plots.
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1106 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/GetAllPosteriorDraws.m
+++ matlab/GetAllPosteriorDraws.m
@@ -9,20 +9,21 @@ DirectoryName = CheckPath('metropolis');
 Draws = zeros(NumberOfDraws*nblck,1);
 logpo = zeros(NumberOfDraws*nblck,1);
 ipost=0;
-if column<0, 
+if column<=0, 
   column=1;  
   ipost=1;
 end
-
-for file = FirstMhFile:TotalNumberOfMhFile
-  for blck = 1:nblck
+iline0=iline;
+for blck = 1:nblck
+  iline=iline0;
+  for file = FirstMhFile:TotalNumberOfMhFile
     load([DirectoryName '/'  M_.fname '_mh' int2str(file) '_blck' int2str(blck)],'x2','logpo2')
     NumberOfLines = size(x2(iline:end,:),1);
     Draws(linee:linee+NumberOfLines-1) = x2(iline:end,column);
     logpo(linee:linee+NumberOfLines-1) = logpo2(iline:end);
     linee = linee+NumberOfLines;
+    iline = 1;
   end
-  iline = 1;
 end
 
 if ipost,
