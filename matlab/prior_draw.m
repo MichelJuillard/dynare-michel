11da7c6eb2cc4df938432cc507a7f83c098df110
ratto <ratto@ac1d8469-bf42-47a9-8791-bf33cf982152>
2006-11-27 14:07:57 +0000

    Needs persistent p1 and p2; global estim_params abd bayestopt_. Debugged generalised beta prior. index (i) missing somewhere
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1104 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/prior_draw.m
+++ matlab/prior_draw.m
@@ -19,8 +19,8 @@ function pdraw = prior_draw(init,cc)
 %  
 % part of DYNARE, copyright S. Adjemian, M. Juillard (2006)
 % Gnu Public License.
-global M_ options_ estim_params_ bayestopt_ 
-persistent fname npar bounds pshape pmean pstd a b p3 p4 condition
+global estim_params_  bayestopt_
+persistent fname npar bounds pshape pmean pstd a b p1 p2 p3 p4 condition
   
 if init
     nvx  = estim_params_.nvx;
@@ -29,8 +29,6 @@ if init
     ncn  = estim_params_.ncn;
     np   = estim_params_.np ;
     npar = nvx+nvn+ncx+ncn+np;
-%    MhDirectoryName = CheckPath('metropolis');
-%    fname = [ MhDirectoryName '/' M_.fname];
     pshape = bayestopt_.pshape;
     pmean = bayestopt_.pmean;
     pstd  = bayestopt_.pstdev;
@@ -43,37 +41,37 @@ if init
     if nargin == 2
         bounds = cc;
     else
-        bounds = [-Inf Inf];
+        bounds = kron(ones(npar,1),[-Inf Inf]);
     end
     for i = 1:npar
         switch pshape(i)
-	 case 3 % Gaussian prior
-%            b(i) = pstd(i)^2/(pmean(i)-p3(i));
-%            a(i) = (pmean(i)-p3(i))/b(i);
-	 case 1 % Beta prior
-	  mu = (p1(i)-p3(i))/(p4(i)-p3(i));
-	  stdd = p2(i)/(p4(i)-p3(i));
-	  a(i) = (1-mu)*mu^2/stdd^2 - mu;
-	  b(i) = a*(1/mu - 1);        
-	 case 2;%Gamma prior
-	  mu = p1(i)-p3(i);
-	  b(i) = p2(i)^2/mu;
-	  a(i) = mu/b;
-	 case {5,4,6}
-	  % Nothing to do here
-	  %
-	  % 4: Inverse gamma, type 1, prior
-	  %    p2(i) = nu
-	  %    p1(i) = s
-	  % 6: Inverse gamma, type 2, prior
-	  %    p2(i) = nu
-	  %    p1(i) = s
-	  % 5: Uniform prior
-	  %    p3(i) and p4(i) are used.
-	 otherwise
-	  disp('prior_draw :: Error!')
-	  disp('Unknown prior shape.')
-	  return
+          case 3% Gaussian prior
+            b(i) = pstd(i)^2/(pmean(i)-p3(i));
+            a(i) = (pmean(i)-p3(i))/b(i);
+          case 1% Beta prior
+            mu = (p1(i)-p3(i))/(p4(i)-p3(i));
+            stdd = p2(i)/(p4(i)-p3(i));
+            a(i) = (1-mu)*mu^2/stdd^2 - mu;
+            b(i) = a(i)*(1/mu - 1);        
+          case 2;%Gamma prior
+            mu = p1(i)-p3(i);
+            b(i) = p2(i)^2/mu;
+            a(i) = mu/b(i);
+          case {5,4,6}
+            % Nothing to do here
+            %
+            % 4: Inverse gamma, type 1, prior
+            %    p2(i) = nu
+            %    p1(i) = s
+            % 6: Inverse gamma, type 2, prior
+            %    p2(i) = nu
+            %    p1(i) = s
+            % 5: Uniform prior
+            %    p3(i) and p4(i) are used.
+          otherwise
+            disp('prior_draw :: Error!')
+            disp('Unknown prior shape.')
+            return
         end
         pdraw = zeros(npar,1);  
     end
@@ -109,7 +107,8 @@ for i = 1:npar
             y2 = gamma_draw(b(i),1,0);
             tmp = y1/(y1+y2);
             if tmp >= bounds(i,1) && tmp <= bounds(i,2)
-                pdraw(i) = pmean(i)+tmp*pstd(i);
+                %pdraw(i) = pmean(i)+tmp*pstd(i);
+                pdraw(i) = p3(i)+tmp*(p4(i)-p3(i));
                 break
             end
         end
@@ -138,13 +137,13 @@ end
 
   
   
-function gamma_draw(a,b,c)
+function g = gamma_draw(a,b,c)
 % Bauwens, Lubrano & Richard (page 316)
 if a >30
     z = randn;
     g = b*(z+sqrt(4*a-1))^2/4 + c; 
 else
-    condi = 1
+    condi = 1;
     while condi
         x = -1;
         while x<0
