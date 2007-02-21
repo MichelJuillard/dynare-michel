5c285d6fa0b07fc14c66d9af52fcd5a67b52c6a2
sebastien <sebastien@ac1d8469-bf42-47a9-8791-bf33cf982152>
2007-02-21 23:28:38 +0000

    v4 erase_compiled_function.m: merged Ferhat's changes
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1191 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/erase_compiled_function.m
+++ matlab/erase_compiled_function.m
@@ -1,8 +1,10 @@
 function erased_compiled_function(func)
   % erase compiled function with name 'func'
-    
+
     if exist([func '.dll'])
+      clear [func '.dll']
       delete [func '.dll']
     elseif exist ([func '.mexw32'])
+      clear [func '.dll']
       delete [func '.mexw32']
     end
