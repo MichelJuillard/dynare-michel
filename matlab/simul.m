2f7b6c40371bb8ab5a9b5bb7223d2484cad1192e
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2007-12-20 16:32:02 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1508 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/simul.m
+++ matlab/simul.m
@@ -13,7 +13,7 @@ function simul(dr)
 %   none
 %  
 %  
-% part of DYNARE, copyright S. Adjemian, M. Juillard (1996-2006)
+% part of DYNARE, copyright Dynare Team (1996-2007)
 % Gnu Public License.
 
 global M_ options_ oo_ 
