25e3fe8e3b3ef6c3e59068e0ea656b9831a9f9f1
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2007-12-20 15:48:12 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1502 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/simk.m
+++ matlab/simk.m
@@ -17,7 +17,7 @@ function simk
 %   None.
 %  
 %  
-% part of DYNARE, copyright S. Adjemian, M. Juillard (1996-2006)
+% part of DYNARE, copyright Dynare Team (1996-2007)
 % Gnu Public License.
 global M_ options_ oo_
 global it_ iyr0 ct_ broyden_
