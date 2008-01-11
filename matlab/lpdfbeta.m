ac0ab299c1bcd51331d5ac63122dcd59e544b4a5
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-11 15:40:40 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1576 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/lpdfbeta.m
+++ matlab/lpdfbeta.m
@@ -1,5 +1,22 @@
 function ldens = lpdfbeta(x,a,b);
-% log BETA PDF
+
+% function ldens = lpdfbeta(x,a,b)
+% log Beta PDF
+%
+% INPUTS
+%    x:      density evatuated at x
+%    a:      Beta distribution paramerer 
+%    b:      Beta distribution paramerer 
+%    
+% OUTPUTS
+%    ldens:  the log Beta PDF
+%        
+% SPECIAL REQUIREMENTS
+%    none
+%  
+% part of DYNARE, copyright Dynare Team (2003-2008)
+% Gnu Public License.
+
   ldens = gammaln(a+b) - gammaln(a) - gammaln(b) + (a-1)*log(x) + (b-1)*log(1-x);
 
 % 10/11/03 MJ adapted from a GAUSS version by F. Schorfheide, translated
