884ca1c35f8a61206a81ef1b0ba596b6abbdf44b
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-11 15:57:20 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1582 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/lpdfnorm.m
+++ matlab/lpdfnorm.m
@@ -1,7 +1,22 @@
 function  f = lpdfnorm(x,m,s)
-%LPDFNORM The log of the normal density function
+
+% function f = lpdfnorm(x,m,s)
+% The log of the normal density function 
 %
-%         f = lpdfnorm(x,Mean,StandardDeviation)
+% INPUTS
+%    x:      density evatuated at x
+%    m:      mean 
+%    s:      standard deviation 
+
+% OUTPUTS
+%    f:      the log of the normal density function
+%        
+% SPECIAL REQUIREMENTS
+%    none
+%  
+% part of DYNARE, copyright Dynare Team (2003-2008)
+% Gnu Public License.
+
 
 if nargin<3, s=1; end
 if nargin<2, m=0; end
