7bfe33473b60ba357d5058f8c774cc3a3588e31c
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-11 15:49:59 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1580 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/lpdfig1.m
+++ matlab/lpdfig1.m
@@ -1,12 +1,24 @@
 function ldens = lpdfig1(x,s,nu)
-% log INVERSE GAMMA (type 1)
-%
+
+% function ldens = lpdfig1(x,s,nu)
+% log INVERSE GAMMA (type 1) 
 % X ~ IG1(s,nu)
 % X = sqrt(Y) where Y ~ IG2(s,nu) and Y = inv(Z) with Z ~ G(nu/2,2/s) (Gamma distribution) 
-% 
+%
+% INPUTS
+%    x:      density evatuated at x
+%    s:      shape parameter 
+%    nu:     scale parameter 
+
+% OUTPUTS
+%    ldens:  the log INVERSE GAMMA density function (type 1)
+%        
+% SPECIAL REQUIREMENTS
 % See L. Bauwens, M. Lubrano and J-F. Richard [1999, appendix A] for more
 % details.
-%
-% stephane.adjemian@cepremap.cnrs.fr [01/16/2004]
+%  
+% part of DYNARE, copyright Dynare Team (2004-2008)
+% Gnu Public License.
+
 
 ldens = log(2) - gammaln(nu/2) - (nu/2).*log(2/s) - (nu+1)*log(x) - .5*s./(x.^2);
