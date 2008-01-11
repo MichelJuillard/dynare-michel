190f2d14c0371c46b7a9768b3b619643b6af3427
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-11 14:22:41 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1567 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/DiffuseLikelihoodH1.m
+++ matlab/DiffuseLikelihoodH1.m
@@ -1,79 +1,36 @@
 function [LIK, lik] = DiffuseLikelihoodH1(T,R,Q,H,Pinf,Pstar,Y,trend,start)
-% M. Ratto added lik in output
-% stephane.adjemian@cepremap.cnrs.fr [07-19-2004]
+
+% function [LIK, lik] = DiffuseLikelihoodH1(T,R,Q,H,Pinf,Pstar,Y,trend,start)
+% Computes the diffuse likelihood (H=measurement error) in the case of a non-singular var-cov matrix 
 %
+% INPUTS
+%    T:      mm*mm matrix
+%    R:      mm*rr matrix
+%    Q:      rr*rr matrix
+%    H:      pp*pp matrix
+%    Pinf:   mm*mm diagonal matrix with with q ones and m-q zeros
+%    Pstar:  mm*mm variance-covariance matrix with stationary variables
+%    Y:      pp*1 vector
+%    trend
+%    start:  likelihood evaluation at 'start'
+%             
+% OUTPUTS
+%    LIK:    likelihood
+%    lik:    density vector in each period
+%        
+% SPECIAL REQUIREMENTS
 %   See "Filtering and Smoothing of State Vector for Diffuse State Space
 %   Models", S.J. Koopman and J. Durbin (2003, in Journal of Time Series 
-%   Analysis, vol. 24(1), pp. 85-98).  
-%
-%   THE PROBLEM:
-%
-%   y_t =   Z_t * \alpha_t + \varepsilon_t
-%   \alpha_{t+1} = T_t  * \alpha_t + R_t * \eta_t
-%
-%   with:
-%
-%   \alpha_1 = a + A*\delta + R_0*\eta_0
-%
-%   m*q matrix A and m*(m-q) matrix R_0 are selection matrices (their
-%   columns constitue all the columns of the m*m identity matrix) so that 
-%
-%       A'*R_0 = 0 and A'*\alpha_1 = \delta
-%
-%   We assume that the vector \delta is distributed as a N(0,\kappa*I_q)
-%   for a given  \kappa > 0. So that the expectation of \alpha_1 is a and
-%   its variance is P, with
-%
-%       P = \kappa*P_{\infty} + P_{\star}
-%
-%           P_{\infty} = A*A'
-%           P_{\star}  = R_0*Q_0*R_0'
-%
-%   P_{\infty} is a m*m diagonal matrix with q ones and m-q zeros. 
-%
-%
-%   and where:
-%
-%   y_t             is a pp*1 vector
-%   \alpha_t        is a mm*1 vector
-%   \varepsilon_t   is a pp*1 multivariate random variable (iid N(0,H_t))
-%   \eta_t          is a rr*1 multivariate random variable (iid N(0,Q_t))
-%   a_1             is a mm*1 vector
-%
-%   Z_t     is a pp*mm matrix
-%   T_t     is a mm*mm matrix
-%   H_t     is a pp*pp matrix
-%   R_t     is a mm*rr matrix
-%   Q_t     is a rr*rr matrix
-%   P_1     is a mm*mm matrix
-%
-%
-%   FILTERING EQUATIONS:
-%
-%   v_t = y_t - Z_t* a_t
-%   F_t = Z_t * P_t * Z_t' + H_t
-%   K_t = T_t * P_t * Z_t' * F_t^{-1}
-%   L_t = T_t - K_t * Z_t
-%   a_{t+1} = T_t * a_t + K_t * v_t
-%   P_{t+1} = T_t * P_t * L_t' + R_t*Q_t*R_t'
-%
-%
-%   DIFFUSE FILTERING EQUATIONS:
-%
-%   a_{t+1} = T_t*a_t + K_{\infty,t}v_t
-%   P_{\infty,t+1} = T_t*P_{\infty,t}*L_{\infty,t}'
-%   P_{\ast,t+1}  = T_t*P_{\ast,t}*L_{\infty,t}' - K_{\infty,t}*F_{\infty,t}*K_{\ast,t}' + R_t*Q_t*R_t'
-%   K_{\infty,t}   = T_t*P_{\infty,t}*Z_t'*F_{\infty,t}^{-1}
-%   v_t = y_t - Z_t*a_t
-%   L_{\infty,t} = T_t - K_{\infty,t}*Z_t
-%   F_{\infty,t} = Z_t*P_{\infty,t}*Z_t'
-%   K_{\ast,t}  = (T_t*P_{\ast,t}*Z_t' + K_{\infty,t}*F_{\ast,t})*F_{\infty,t}^{-1}
-%   F_{\ast,t}  = Z_t*P_{\ast,t}*Z_t' + H_t
-%
-%	Matrix Finf is assumed to be non singular. If this is not the case we have
-%   to switch to another algorithm (NewAlg=3).
-%
-%	start = options_.presample
+%   Analysis, vol. 24(1), pp. 85-98). 
+%  
+% part of DYNARE, copyright Dynare Team (2005-2008)
+% Gnu Public License.
+
+
+
+
+% M. Ratto added lik in output
+
   global bayestopt_ options_
   
   mf = bayestopt_.mf;
