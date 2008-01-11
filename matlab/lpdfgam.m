13029cf258b46da948db5ef4a91b4a85ac9a5a1c
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-11 15:42:27 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1577 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/lpdfgam.m
+++ matlab/lpdfgam.m
@@ -1,5 +1,22 @@
 function  ldens = lpdfgam(x,a,b);
-% log GAMMA PDF 
+
+% function ldens = lpdfbeta(x,a,b)
+% log GAMMA PDF
+%
+% INPUTS
+%    x:      density evatuated at x
+%    a:      GAMMA distribution paramerer 
+%    b:      GAMMA distribution paramerer 
+%    
+% OUTPUTS
+%    ldens:  the log GAMMA PDF
+%        
+% SPECIAL REQUIREMENTS
+%    none
+%  
+% part of DYNARE, copyright Dynare Team (2003-2008)
+% Gnu Public License.
+
   ldens = -gammaln(a) -a*log(b)+ (a-1)*log(x) -x/b ;
 
 % 10/11/03  MJ adapted from an earlier GAUSS version by F. Schorfeide,
