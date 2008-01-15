38ecaa2f2026dafff493698da9f85c7446035289
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-15 13:15:18 +0000

    isolated functions deleted
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1590 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/CreateBenchmark.m
+++
@@ -1,8 +0,0 @@
-function CreateBenchmark
-
-% stephane.adjemian@cepremap.cnrs.fr [12-06-2004]
-
-global oo_
-
-eval([M_.fname '_oo_ = oo_;'])
-eval(['save ' M_.fname '_benchmark_oo ' M_.fname '_oo_'])
--- matlab/fnorm.m
+++
@@ -1,2 +0,0 @@
-function e = fnorm(p2,p,p1,perc)
-  e = p - pnorm(perc,p1,p2);
