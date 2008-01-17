125f61e4c5135a8c2e6819e24ec221d60ad3e20a
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-17 16:00:51 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1610 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/save_results.m
+++ matlab/save_results.m
@@ -1,19 +1,22 @@
 function save_results(x,s_name,names)
+
 % function save_results(x,s_name,names)
 % save results in appropriate structure
+%
 % INPUT
 %   x: matrix to be saved column by column
 %   s_name: name of the structure where to save the results
 %   names: names of the individual series
+%
 % OUTPUT
 %   none
-% ALGORITHM
-%   none
+%
 % SPECIAL REQUIREMENT
 %   none
 %    
-% part of DYNARE, copyright S. Adjemian, M. Juillard (2006)
+% part of DYNARE, copyright Dynare Team (2006-2008)
 % Gnu Public License.
+
   global oo_
   
   for i=1:size(x,2)
