473161027bdd19ade77571d2fdc096c9519f5d6e
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-17 11:06:49 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1598 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/qr2.m
+++ matlab/qr2.m
@@ -1,4 +1,6 @@
 function [Q,R] = qr2(X)
+
+% function [Q,R] = qr2(X)
 % This routine performs a qr decomposition of matrix X such that the
 % diagonal scalars of the upper-triangular matrix R are positive. If X 
 % is a full (column) rank matrix, then R is also the cholesky
@@ -17,10 +19,9 @@ function [Q,R] = qr2(X)
 % SPECIAL REQUIREMENTS
 %   None.
 %  
-%  
-% stephane.adjemian@ens.fr [12-07-2005]
 % part of DYNARE, copyright Dynare Team (2007)
 % Gnu Public License.
+
 [Q,R] = qr(X);
 indx = find(diag(R)<0);
 if ~isempty(indx)
