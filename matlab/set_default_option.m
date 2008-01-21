e2f263da105390a97172016a5ac4e23df3f2f1dd
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-21 11:01:12 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1624 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/set_default_option.m
+++ matlab/set_default_option.m
@@ -1,4 +1,22 @@
 function options=set_default_option(options,field,default)
+
+% function options=set_default_option(options,field,default)
+% Sets the option value 
+% 
+% INPUTS
+%    options
+%    field:   option name
+%    default: assigns a value
+%    
+% OUTPUTS
+%    options
+%        
+% SPECIAL REQUIREMENTS
+%    none
+%  
+% part of DYNARE, copyright Dynare Team (2003-2008)
+% Gnu Public License.
+
   if ~isfield(options,field)
     if sscanf(version('-release'),'%d') < 13
       options = setfield(options,field,default);
