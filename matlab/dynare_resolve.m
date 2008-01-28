b0760493ae9d0b5e37cc477ffd2bcefe1e486980
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-28 14:46:20 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1653 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/dynare_resolve.m
+++ matlab/dynare_resolve.m
@@ -1,4 +1,34 @@
 function [A,B,ys,info] = dynare_resolve(iv,ic,aux)
+
+% function [A,B,ys,info] = dynare_resolve(iv,ic,aux)
+% Computes the linear approximation and the matrices A and B of the
+% transition equation
+%
+% INPUTS
+%    iv:             selected variables (observed and state variables)
+%    ic:             state variables position in the transition matrix columns
+%    aux:            indices for auxiliary equations
+%
+% OUTPUTS
+%    A:              matrix of predetermined variables effects in linear solution (ghx)
+%    B:              matrix of shocks effects in linear solution (ghu)
+%    ys:             steady state of original endogenous variables
+%    info=1:         the model doesn't determine the current variables '...' uniquely
+%    info=2:         MJDGGES returns the following error code'
+%    info=3:         Blanchard Kahn conditions are not satisfied: no stable '...' equilibrium
+%    info=4:         Blanchard Kahn conditions are not satisfied:'...' indeterminacy
+%    info=5:         Blanchard Kahn conditions are not satisfied:'...' indeterminacy due to rank failure
+%    info=11:        same as dr1 for dr_algo = 2
+%    info=20:        can't find steady state info(2) contains sum of sqare residuals
+%    info=30:        variance can't be computed
+%
+% SPECIAL REQUIREMENTS
+%    none
+%  
+% part of DYNARE, copyright Dynare Team (2003-2007)
+% Gnu Public License.
+
+
   global oo_ M_
   
   [oo_.dr,info] = resol(oo_.steady_state,0);
