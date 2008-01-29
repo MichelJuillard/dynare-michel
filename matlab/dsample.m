8e5cfdcf2ad904ee91d642f68bc59b2e3097b834
assia <assia@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-01-29 13:46:45 +0000

    header updated
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1668 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/dsample.m
+++ matlab/dsample.m
@@ -1,17 +1,25 @@
-% Copyright (C) 2001 Michel Juillard
-%
 function dsample(s1,s2)
-% DSAMPLE :	DSAMPLE(d1,d2)
-%		This optional command permits to reduce the number of
-%		periods considered in following output commands. 
-%               If only one argument is 
-%		provided, output is from period 1 to the period 
-%		specified in the DSAMPLE command. If two arguments are
-%		present output is done for the interval between the 
-%		two periods.
-%               DSAMPLE without arguments reset the sample to the one 
-%               specified by PERIODS 
 		
+% function dsample(s1,s2)
+% This optional command permits to reduce the number of periods considered in following output commands.
+% If only one argument is provided, output is from period 1 to the period specified in the DSAMPLE command. 
+% If two arguments are present output is done for the interval between the two periods.
+% DSAMPLE without arguments reset the sample to the one specified by PERIODS
+%
+% INPUTS
+%    s1:      first period
+%    s2:      last period
+%    
+% OUTPUTS
+%    none
+%    
+% SPECIAL REQUIREMENTS
+%    none
+%  
+% part of DYNARE, copyright Dynare Team (2001-2008)
+% Gnu Public License.
+
+
 global options_
 
 options_.smpl = zeros(2,1) ;
