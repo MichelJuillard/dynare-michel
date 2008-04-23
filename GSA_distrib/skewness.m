42ed6e75292dd8ed057c3b033a6470ff0105e47e
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2008-04-23 12:49:02 +0000

    fixed bugs
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@30 f1850c17-3b45-254b-b221-fcb05880fee1

--- GSA_distrib/skewness.m
+++ GSA_distrib/skewness.m
@@ -1,2 +1,7 @@
-function s=skewness(y)
-    s=sum((y-mean(y).^3));
+function s=skewness(y),
+
+% y=stand_(y);
+% s=mean(y.^3);
+    m2=mean((y-mean(y)).^2);
+    m3=mean((y-mean(y)).^3);
+    s=m3/m2^1.5;
