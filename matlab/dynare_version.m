b99d608b7e9086f8f57e73ca8ee476999c522df6
michel <michel@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-05-01 13:34:14 +0000

    v4 adding dynare_version.m
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@1813 ac1d8469-bf42-47a9-8791-bf33cf982152

---
+++ matlab/dynare_version.m
@@ -0,0 +1,18 @@
+function dynare_version
+
+% function dynare_version
+%	This command returns Dynare version number
+%
+% INPUTS
+%   none
+%             
+% OUTPUTS
+%   Dynare version number
+%        
+% SPECIAL REQUIREMENTS
+%   none
+%  
+% part of DYNARE, copyright Dynare Team (2001-2008)
+% Gnu Public License.
+
+    return '4-Prerelease'
