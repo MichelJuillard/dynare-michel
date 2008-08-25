469eb87f40036c824f62f1874259601f96d972ad
adjemian <adjemian@ac1d8469-bf42-47a9-8791-bf33cf982152>
2008-08-25 21:09:49 +0000

    Cosmetic change for Junior.
    git-svn-id: https://www.dynare.org/svn/dynare/dynare_v4@2001 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/csminit.m
+++ matlab/csminit.m
@@ -119,7 +119,7 @@ else
          dxtest=x0+dx*lambda;
       end
       % home
-      f = eval([fcn '(dxtest,varargin{:})']);
+      f = feval(fcn,dxtest,varargin{:});
       %ARGLIST
       %f = feval(fcn,dxtest,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13);
       % f = feval(fcn,x0+dx*lambda,P1,P2,P3,P4,P5,P6,P7,P8);
