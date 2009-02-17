74c8e53140d9db1fccc08a61ad901836f9f5b2e9
stepan <stepan@ac1d8469-bf42-47a9-8791-bf33cf982152>
2009-02-17 17:21:05 +0000

    v4.1:: Vectorization of the prior sampling algorithm.
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@2424 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/prior_draw.m
+++ matlab/prior_draw.m
@@ -1,21 +1,24 @@
-function pdraw = prior_draw(init,cc)
-% function pdraw = prior_draw(init,cc)
-% Builds one draw from the prior distribution. 
+function pdraw = prior_draw(init, cc, npar, prior_structure)
+% This function generate one draw from the joint prior distribution.
 % 
-% INPUTS
-%   o init           [integer]  scalar equal to 1 (first call) or 0.
-%   o cc             [double]   two columns matrix (same as in
-%                               metropolis.m), constraints over the
-%                               parameter space (upper and lower bounds).
-%    
+% INPUTS 
+%   o init             [integer]    scalar equal to 1 (first call) or 0.
+%   o cc               [double]     two columns matrix (same as in
+%                                   metropolis.m), constraints over the
+%                                   parameter space (upper and lower bounds).
+%   o npar             [integer]    number of estimated parameters.
+%   o prior_structure  [structure]  Describes the prior distribution [bayestopt_]
+%
 % OUTPUTS 
-%   o pdraw          [double]   draw from the joint prior density. 
+%   o pdraw            [double]     npar*1 vector, draws from the joint prior density.
 %
 %
 % SPECIAL REQUIREMENTS
 %   none
+%
+% NOTE Input arguments 1,2,3 and 4 are only needed for initialization.
 
-% Copyright (C) 2006-2008 Dynare Team
+% Copyright (C) 2006-2009 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -32,111 +35,139 @@ function pdraw = prior_draw(init,cc)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
-global estim_params_  bayestopt_
-persistent fname npar bounds pshape pmean pstd a b p1 p2 p3 p4 condition
-  
-if init
-    nvx  = estim_params_.nvx;
-    nvn  = estim_params_.nvn;
-    ncx  = estim_params_.ncx;
-    ncn  = estim_params_.ncn;
-    np   = estim_params_.np ;
-    npar = nvx+nvn+ncx+ncn+np;
-    pshape = bayestopt_.pshape;
-    pmean = bayestopt_.pmean;
-    pstd  = bayestopt_.pstdev;
-    p1 = bayestopt_.p1;
-    p2 = bayestopt_.p2;
-    p3 = bayestopt_.p3;
-    p4 = bayestopt_.p4;
-    a = zeros(npar,1);
-    b = zeros(npar,1); 
-    if nargin == 2
+persistent number_of_estimated_parameters prior_shape prior_mean prior_standard_deviation a b p1 p2 p3 p4
+persistent bounds nobounds_flag
+persistent uniform_index gaussian_index gamma_index beta_index inverse_gamma_1_index inverse_gamma_2_index
+
+if nargin>0 && init
+    number_of_estimated_parameters = npar;
+    prior_shape = prior_structure.pshape;
+    prior_mean = prior_structure.pmean;
+    prior_standard_deviation  = prior_structure.pstdev;
+    p1 = prior_structure.p1;
+    p2 = prior_structure.p2;
+    p3 = prior_structure.p3;
+    p4 = prior_structure.p4;
+    a = NaN(number_of_estimated_parameters,1);
+    b = NaN(number_of_estimated_parameters,1); 
+    if ~isempty(cc)
         bounds = cc;
+        nobounds_flag = 0;
     else
-        bounds = kron(ones(npar,1),[-Inf Inf]);
+        nobounds_flag = 1;
     end
-    for i = 1:npar
-        switch pshape(i)
-          case 3% Gaussian prior
-            b(i) = pstd(i)^2/(pmean(i)-p3(i));
-            a(i) = (pmean(i)-p3(i))/b(i);
-          case 1% Beta prior
-            mu = (p1(i)-p3(i))/(p4(i)-p3(i));
-            stdd = p2(i)/(p4(i)-p3(i));
-            a(i) = (1-mu)*mu^2/stdd^2 - mu;
-            b(i) = a(i)*(1/mu - 1);        
-          case 2;%Gamma prior
-            mu = p1(i)-p3(i);
-            b(i) = p2(i)^2/mu;
-            a(i) = mu/b(i);
-          case {5,4,6}
-            % Nothing to do here
-            %
-            % 4: Inverse gamma, type 1, prior
-            %    p2(i) = nu
-            %    p1(i) = s
-            % 6: Inverse gamma, type 2, prior
-            %    p2(i) = nu
-            %    p1(i) = s
-            % 5: Uniform prior
-            %    p3(i) and p4(i) are used.
-          otherwise
-           error(sprintf('prior_draw: unknown distribution shape (index %d, type %d)', i, pshape(i)));
-        end
-        pdraw = zeros(npar,1);  
-    end
-    condition = 1;
-    pdraw = zeros(npar,1);
+    beta_index = find(prior_shape==1);
+    gamma_index = find(prior_shape==2);
+    gaussian_index = find(prior_shape==3);
+    inverse_gamma_1_index = find(prior_shape==4);
+    uniform_index = find(prior_shape==5);
+    inverse_gamma_2_index = find(prior_shape==6);
+    % Set parameters for the beta prior
+    mu = (p1(beta_index)-p3(beta_index))./(p4(beta_index)-p3(beta_index));
+    stdd = p2(beta_index)./(p4(beta_index)-p3(beta_index));
+    a(beta_index) = (1-mu).*mu.^2./stdd.^2 - mu;
+    b(beta_index) = a(beta_index).*(1./mu - 1);
+    % Set parameters for the gamma prior
+    mu = p1(gamma_index)-p3(gamma_index);
+    b(gamma_index) = p2(gamma_index).^2./mu;
+    a(gamma_index) = mu./b(gamma_index);
+    % Initialization of the vector of prior draws.
+    pdraw = zeros(number_of_estimated_parameters,1);
     return
 end
 
-
-for i = 1:npar
-    switch pshape(i)
-      case 5% Uniform prior.
-        pdraw(i) = rand*(p4(i)-p3(i)) + p3(i);
-      case 3% Gaussian prior.
-        while condition
-            tmp = randn*pstd(i) + pmean(i);
-            if tmp >= bounds(i,1) && tmp <= bounds(i,2)
-                pdraw(i) = tmp;
-                break
-            end
+if nobounds_flag
+    % Uniform draws.
+    if ~isempty(uniform_index)
+        pdraw(uniform_index) = rand(length(uniform_index),1).*(p4(uniform_index)-p3(uniform_index)) + p3(uniform_index);  
+    end
+    % Gaussian draws.
+    if ~isempty(gaussian_index)
+        pdraw(gaussian_index) = randn(length(gaussian_index),1).*prior_standard_deviation(gaussian_index) + prior_mean(gaussian_index);
+    end
+    % Gamma draws.
+    if ~isempty(gamma_index)
+        pdraw(gamma_index) = gamrnd(a(gamma_index),b(gamma_index))+p3(gamma_index);
+    end
+    % Beta draws.
+    if ~isempty(beta_index)
+        pdraw(beta_index) = (p4(beta_index)-p3(beta_index)).*betarnd(a(beta_index),b(beta_index))+p3(beta_index);
+    end
+    % Inverted gamma (type 1) draws.
+    if ~isempty(inverse_gamma_1_index)
+        pdraw(inverse_gamma_1_index) = ...
+            sqrt(1./gamrnd(p2(inverse_gamma_1_index)/2,2./p1(inverse_gamma_1_index)));
+    end
+    % Inverted gamma (type 2) draws.
+    if ~isempty(inverse_gamma_2_index)
+        pdraw(inverse_gamma_2_index) = ...
+            1./gamrnd(p2(inverse_gamma_2_index)/2,2./p1(inverse_gamma_2_index));
+    end
+else
+    if ~isempty(uniform_index)
+        mm = length(uniform_index);
+        id = 1:mm;
+        ID = 1:mm;
+        while mm
+            pdraw(uniform_index(id)) = rand(mm,1).*(p4(uniform_index(id))-p3(uniform_index(id))) + p3(uniform_index(id));
+            id = ID( find( (pdraw(uniform_index(id))<bounds(uniform_index(id),1)) || ...
+                           (pdraw(uniform_index(id))>bounds(uniform_index(id),2)) ) ) ;
+            mm = length(id);
         end
-      case 2% Gamma prior.
-        while condition
-            g = gamrnd(a(i),b(i)) + p3(i);
-            if g >= bounds(i,1) && g <= bounds(i,2)
-                pdraw(i) = g;
-                break
-            end
+    end
+    if ~isempty(gaussian_index)
+        mm = length(gaussian_index);
+        id = 1:mm;
+        ID = 1:mm;
+        while mm
+            pdraw(gaussian_index(id)) = randn(mm,1).*prior_standard_deviation(gaussian_index(id)) + prior_mean(gaussian_index(id));
+            id = ID( find( (pdraw(gaussian_index(id))<bounds(gaussian_index(id),1)) || ...
+                           (pdraw(gaussian_index(id))>bounds(gaussian_index(id),2)) ) ) ;
+            mm = length(id);
         end
-      case 1% Beta distribution
-        while condition
-            tmp = betarnd(a(i), b(i));
-            if tmp >= bounds(i,1) && tmp <= bounds(i,2)
-                pdraw(i) = p3(i)+tmp*(p4(i)-p3(i));
-                break
-            end
+    end
+    if ~isempty(gamma_index)
+        mm = length(gamma_index);
+        id = 1:mm;
+        ID = 1:mm;
+        while mm
+            pdraw(gamma_index(id)) = gamrnd(a(gamma_index(id)),b(gamma_index(id)))+p3(gamma_index(id));
+            id = ID( find( (pdraw(gamma_index(id))<bounds(gamma_index(id),1)) || ...
+                           (pdraw(gamma_index(id))>bounds(gamma_index(id),2)) ) ) ;
+            mm = length(id);
         end
-      case 4% INV-GAMMA1 distribution
-        while condition
-            tmp = sqrt(1/gamrnd(p2(i)/2,2/p1(i)));
-            if tmp >= bounds(i,1) && tmp <= bounds(i,2)
-                pdraw(i) = tmp;
-                break
-            end
-        end        
-      case 6% INV-GAMMA2 distribution  
-        while condition
-            tmp = 1/gamrnd(p2(i)/2,2/p1(i));
-            if tmp >= bounds(i,1) && tmp <= bounds(i,2)
-                pdraw(i) = tmp;
-                break
-            end
+    end
+    if ~isempty(beta_index)
+        mm = length(beta_index);
+        id = 1:mm;
+        ID = 1:mm;
+        while mm
+            pdraw(beta_index(id)) = (p4(beta_index(id))-p3(beta_index(id))).*betarnd(a(beta_index(id)),b(beta_index(id)))+p3(beta_index(id));
+            id = ID( find( (pdraw(beta_index(id))<bounds(beta_index(id),1)) || ...
+                           (pdraw(beta_index(id))>bounds(beta_index(id),2)) ) ) ;
+            mm = length(id);
         end
-      otherwise
-       error(sprintf('prior_draw: unknown distribution shape (index %d, type %d)', i, pshape(i)));
+    end
+    if ~isempty(inverse_gamma_1_index)
+        mm = length(inverse_gamma_1_index);
+        id = 1:mm;
+        ID = 1:mm;
+        while mm
+            pdraw(inverse_gamma_1_index(id)) = sqrt(1./gamrnd(p2(inverse_gamma_1_index(id))/2,2./p1(inverse_gamma_1_index(id))));
+            id = ID( find( (pdraw(inverse_gamma_1_index(id))<bounds(inverse_gamma_1_index(id),1)) || ...
+                           (pdraw(inverse_gamma_1_index(id))>bounds(inverse_gamma_1_index(id),2)) ) ) ;
+            mm = length(id);
+        end
+    end
+    if ~isempty(inverse_gamma_2_index)
+        mm = length(inverse_gamma_2_index);
+        id = 1:mm;
+        ID = 1:mm;
+        while mm
+            pdraw(inverse_gamma_2_index(id)) = 1./gamrnd(p2(inverse_gamma_2_index(id))/2,2./p1(inverse_gamma_2_index(id)));
+            id = ID( find( (pdraw(inverse_gamma_2_index(id))<bounds(inverse_gamma_2_index(id),1)) || ...
+                           (pdraw(inverse_gamma_2_index(id))>bounds(inverse_gamma_2_index(id),2)) ) ) ;
+            mm = length(id);
+        end
     end
 end
