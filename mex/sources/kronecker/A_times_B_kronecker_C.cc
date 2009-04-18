MichelJuillard/dynare-michel

commit 9fa3457791c4a31450db491ee03848ded4b625b5

4.1: fixing again compatibility for Matlab 7.8      adding matlab_versions_compatibility.h to start factorizing necessary code
git-svn-id: https://www.dynare.org/svn/dynare/trunk@2577 ac1d8469-bf42-47a9-8791-bf33cf982152

mex/sources/kronecker/A_times_B_kronecker_C.cc
@@ -24,6 +24,7 @@
 
 #include <string.h>
 #include "mex.h"
+#include "../matlab_versions_compatibility.h"
 
 #ifdef NO_OPENMP
   #define USE_OMP 0
@@ -33,7 +34,7 @@
       #define dgemm dgemm_
     #endif
     extern "C" {
-    int dgemm(char*, char*, int*, int*, int*, double*, double*, int*, double*, int*, double*, double*, int*);
+    lapack_int dgemm(char*, char*, lapack_int*, lapack_int*, lapack_int*, double*, double*, lapack_int*, double*, lapack_int*, double*, double*, lapack_int*);
     }
   #else /* NO_BLAS_H */
     #include "blas.h"
@@ -50,7 +51,7 @@ typedef int mwSize;
 #endif
 
 void full_A_times_kronecker_B_C(double *A, double *B, double *C, double *D,
-				int mA, int nA, int mB, int nB, int mC, int nC)
+				lapack_int mA, lapack_int nA, lapack_int mB, lapack_int nB, lapack_int mC, lapack_int nC)
 {
   #if USE_OMP
     #pragma omp parallel for num_threads(atoi(getenv("DYNARE_NUM_THREADS")))
@@ -94,7 +95,7 @@ void full_A_times_kronecker_B_C(double *A, double *B, double *C, double *D,
 }
 
 
-void full_A_times_kronecker_B_B(double *A, double *B, double *D, int mA, int nA, int mB, int nB)
+void full_A_times_kronecker_B_B(double *A, double *B, double *D, lapack_int mA, lapack_int lapack_nA, lapack_int mB, lapack_int nB)
 {
   #if USE_OMP
     #pragma omp parallel for num_threads(atoi(getenv("DYNARE_NUM_THREADS")))

mex/sources/matlab_versions_compatibility.h
@@ -1,0 +1,10 @@
+#if !defined(MATLAB_VERSIONS_COMPATIBILITY_H)
+#define MATLAB_VERSIONS_COMPATIBILITY_H
+
+#if !defined(LAPACK_USE_MWSIGNEDINDEX) || defined(OCTAVE)
+typedef int lapack_int;
+#else
+typedef mwSignedIndex lapack_int;
+#endif
+
+#endif

mex/sources/mjdgges/mjdgges.c
@@ -19,12 +19,7 @@
 
 #include <string.h>
 #include "mex.h"
-
-#if !defined(LAPACK_USE_MWSIGNEDINDEX) || defined(OCTAVE)
-typedef int lapack_int;
-#else
-typedef mwSignedIndex lapack_int;
-#endif
+#include "../matlab_versions_compatibility.h"
 
 #ifdef NO_LAPACK_H
 # if defined(__linux__) || defined(OCTAVE)

commit 2fac4b624faf7ff5c970884a210e6d930e94f05c

trunk mex/A_times_B_kronecker_C: fixed typo introduced in r2577
git-svn-id: https://www.dynare.org/svn/dynare/trunk@2603 ac1d8469-bf42-47a9-8791-bf33cf982152

mex/sources/kronecker/A_times_B_kronecker_C.cc
@@ -95,7 +95,7 @@ void full_A_times_kronecker_B_C(double *A, double *B, double *C, double *D,
 }
 
 
-void full_A_times_kronecker_B_B(double *A, double *B, double *D, lapack_int mA, lapack_int lapack_nA, lapack_int mB, lapack_int nB)
+void full_A_times_kronecker_B_B(double *A, double *B, double *D, lapack_int mA, lapack_int nA, lapack_int mB, lapack_int nB)
 {
   #if USE_OMP
     #pragma omp parallel for num_threads(atoi(getenv("DYNARE_NUM_THREADS")))