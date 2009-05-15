2d846f0da7b70df13276ffe1c2eb9c6ce15354d4
ratto <ratto@ac1d8469-bf42-47a9-8791-bf33cf982152>
2009-05-15 16:38:18 +0000

    1) Fixed problems in file exchange when multiple computers are used at the same time; 2) contains traps for unix, with commented instructions to be `translated' appropriately for the unix environment.
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@2677 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/fMessageStatus.m
+++ matlab/fMessageStatus.m
@@ -1,12 +1,16 @@
 function fMessageStatus(prtfrc, njob, waitbarString, waitbarTitle, Local, MasterName, DyMo)
 
-global fname
+global funcName
 
 if nargin<4,
   Local=1;
 end
 
-save(['comp_status_',fname,int2str(njob)],'prtfrc','njob','waitbarString','waitbarTitle');
+save(['comp_status_',funcName,int2str(njob)],'prtfrc','njob','waitbarString','waitbarTitle');
 if Local==0,
-  copyfile(['comp_status_',fname,int2str(njob),'.mat'],['\\',MasterName,'\',DyMo(1),'$\',DyMo(4:end),'\']);
+  if isunix,
+%     copyfile(['comp_status_',funcName,int2str(njob),'.mat'],['\\',MasterName,'\',DyMo(1),'$\',DyMo(4:end),'\']);
+  else
+    copyfile(['comp_status_',funcName,int2str(njob),'.mat'],['\\',MasterName,'\',DyMo(1),'$\',DyMo(4:end),'\']);
+  end
 end
--- matlab/fParallel.m
+++ matlab/fParallel.m
@@ -1,13 +1,23 @@
 
 function fparallel(fblck,nblck,whoiam,ThisMatlab,fname);
 
+global funcName
+
+funcName=fname;
+
+warning off;
+diary off;
+
+delete( [fname,'_',int2str(whoiam),'.log']);
+
+diary( [fname,'_',int2str(whoiam),'.log']);
+
     
 % configure dynare environment
 dynareroot = dynare_config();
 
 load( [fname,'_input']) 
 % keyboard;
-global fname
 if exist('fGlobalVar'),
   globalVars = fieldnames(fGlobalVar);
   for j=1:length(globalVars),
@@ -17,8 +27,9 @@ if exist('fGlobalVar'),
 end
 
 % lounch the routine to be run in parallel
-fOutputVar = feval(fname, fInputVar ,fblck, nblck, whoiam, ThisMatlab);
-
+tic,
+[fOutputVar, OutputFileName] = feval(fname, fInputVar ,fblck, nblck, whoiam, ThisMatlab);
+toc,
 
 %%% Sincronismo "Esterno" %%%%%%%%%%%%%
 %%% Ogni Processo quando ha finito lo notifica cancellando un file ... 
@@ -29,12 +40,25 @@ if(whoiam)
   
   if options_.parallel(ThisMatlab).Local
     delete(['P_',fname,'_',int2str(whoiam),'End.txt']);
+
   else
-    delete(['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end),'\P_',fname,'_',int2str(whoiam),'End.txt']);
-   
+    if isunix,
+%     for j=1:size(OutputFileName,1),
+%       copyfile([OutputFileName{j,1},OutputFileName{j,2}],['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end),'\',OutputFileName{j,1}])
+%     end
+%     copyfile([fname,'_output_',int2str(whoiam),'.mat'],['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end)]);
+%     delete(['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end),'\P_',fname,'_',int2str(whoiam),'End.txt']);
+    else
+      for j=1:size(OutputFileName,1),
+        copyfile([OutputFileName{j,1},OutputFileName{j,2}],['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end),'\',OutputFileName{j,1}])
+      end
+      copyfile([fname,'_output_',int2str(whoiam),'.mat'],['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end)]);
+      delete(['\\',fInputVar.MasterName,'\',fInputVar.DyMo(1),'$\',fInputVar.DyMo(4:end),'\P_',fname,'_',int2str(whoiam),'End.txt']);
+    end
   end
 end
 
-
+disp(['fParallel ',int2str(whoiam),' completed.'])
+diary off;
 
 exit;
