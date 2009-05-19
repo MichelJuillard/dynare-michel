6f5d4b3c95f644b761569064603ad70da3bc2e44
sebastien <sebastien@ac1d8469-bf42-47a9-8791-bf33cf982152>
2009-05-19 08:57:07 +0000

    trunk: fixed several copyright headers
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@2681 ac1d8469-bf42-47a9-8791-bf33cf982152

--- mex/sources/korderpert/src/k_order_perturbation.h
+++ mex/sources/korderpert/src/k_order_perturbation.h
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2008-2009 Dynare Team
-*
-* This file is part of Dynare.
-*
-* Dynare is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Dynare is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2008-2009 Dynare Team
+ *
+ * This file is part of Dynare.
+ *
+ * Dynare is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dynare is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // The following ifdef block is the standard way of creating macros which make exporting 
 // from a DLL simpler. All files within this DLL are compiled with the K_ORDER_PERTURBATION_EXPORTS
