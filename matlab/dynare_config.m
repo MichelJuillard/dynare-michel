85ac76b9ae779d67414fca9adcdfbf5cefba2546
sebastien <sebastien@ac1d8469-bf42-47a9-8791-bf33cf982152>
2009-09-03 16:10:41 +0000

    Add isempty to regexp to get a boolean answer for a 32-bit systems
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@2894 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/dynare_config.m
+++ matlab/dynare_config.m
@@ -76,7 +76,7 @@ if exist('OCTAVE_VERSION')
 else
     if matlab_ver_less_than('7.5')
         path_to_mex_files = [dynareroot '../mex/2007a/'] ;
-    elseif matlab_ver_less_than('7.8') || ~regexp(computer, '.*64')
+    elseif matlab_ver_less_than('7.8') || isempty(regexp(computer, '.*64', 'ONCE'))
         path_to_mex_files = [dynareroot '../mex/2007b/'] ;
     else
         path_to_mex_files = [dynareroot '../mex/2009a-64bit/'] ;
