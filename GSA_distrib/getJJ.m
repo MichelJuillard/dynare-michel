d33e600667d04e7aa685532cf008ee63fa4caa6a
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2009-09-22 10:08:08 +0000

    Added derivatives to autocorrelations
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@58 f1850c17-3b45-254b-b221-fcb05880fee1

--- GSA_distrib/getJJ.m
+++ GSA_distrib/getJJ.m
@@ -1,13 +1,16 @@
-function JJ = getJJ(M_,oo_,options_,kronflag,indx,mf)
+function JJ = getJJ(M_,oo_,options_,kronflag,indx,mf,nlags)
+
+if nargin<5 | isempty(indx), indx = [1:M_.param_nbr];, end,
+if nargin<7 | isempty(nlags), nlags=3; end,
 
 if kronflag == -1,
   fun = 'thet2tau';
   params0 = M_.params;
-  JJ = fdjac(fun,M_.params(indx),indx,1,mf);
+  JJ = fdjac(fun,M_.params(indx),indx,1,mf,nlags);
   assignin('base','M_', M_);
   assignin('base','oo_', oo_);
 else
-  [H, A, B] = getH(M_,oo_,kronflag,indx);
+  [H, A, B, dA, dOm] = getH(M_,oo_,kronflag,indx);
   m = length(A);
   Dm = duplication(m);
   dA = reshape(H(1:m^2,:),[m m length(indx)]);
@@ -15,9 +18,17 @@ else
   dOm = reshape(dOm,[m m length(indx)]);
 
   GAM =  lyapunov_symm(A,B*M_.Sigma_e*B',options_.qz_criterium,options_.lyapunov_complex_threshold);
-
+  
   for j=1:length(indx),
-    dum =  lyapunov_symm(A,(squeeze(dA(:,:,j))*GAM*A'+A*GAM*squeeze(dA(:,:,j))'+squeeze(dOm(:,:,j))),options_.qz_criterium,options_.lyapunov_complex_threshold);
-    JJ(:,j) = vech(dum(mf,mf));
+    dum =  lyapunov_symm(A,dA(:,:,j)*GAM*A'+A*GAM*dA(:,:,j)'+dOm(:,:,j),options_.qz_criterium,options_.lyapunov_complex_threshold);
+    dumm = vech(dum(mf,mf));
+    for i=1:nlags,
+      dum1 = A^i*dum;
+      for ii=1:i,
+        dum1 = dum1 + A^(ii-1)*dA(:,:,j)*A^(i-ii)*GAM;
+      end
+      dumm = [dumm; vec(dum1(mf,mf))];
+    end
+    JJ(:,j) = dumm;
   end
 end
