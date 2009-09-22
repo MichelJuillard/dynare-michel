3ded4f4b73fd90ed0707ba844ee18bc28f78b9af
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2009-09-22 14:22:23 +0000

    Added estimated exogenous shocks
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@63 f1850c17-3b45-254b-b221-fcb05880fee1

--- GSA_distrib/getJJ.m
+++ GSA_distrib/getJJ.m
@@ -1,34 +1,55 @@
-function JJ = getJJ(M_,oo_,options_,kronflag,indx,mf,nlags)
+function JJ = getJJ(M_,oo_,options_,kronflag,indx,indexo,mf,nlags)
 
 if nargin<5 | isempty(indx), indx = [1:M_.param_nbr];, end,
-if nargin<7 | isempty(nlags), nlags=3; end,
+if nargin<6 | isempty(indexo), indexo = [];, end,
+if nargin<8 | isempty(nlags), nlags=3; end,
 
 if kronflag == -1,
   fun = 'thet2tau';
   params0 = M_.params;
-  JJ = fdjac(fun,M_.params(indx),indx,1,mf,nlags);
+  JJ = fdjac(fun,[sqrt(diag(M_.Sigma_e(indexo,indexo))); M_.params(indx)],indx,indexo,1,mf,nlags);
+  M_.params = params0;
   assignin('base','M_', M_);
   assignin('base','oo_', oo_);
 else
-  [H, A, B, dA, dOm] = getH(M_,oo_,kronflag,indx);
+  [H, A, B, dA, dOm] = getH(M_,oo_,kronflag,indx,indexo);
   m = length(A);
-  Dm = duplication(m);
-  dA = reshape(H(1:m^2,:),[m m length(indx)]);
-  dOm = Dm*H(m^2+1:end,:);
-  dOm = reshape(dOm,[m m length(indx)]);
 
-  GAM =  lyapunov_symm(A,B*M_.Sigma_e*B',options_.qz_criterium,options_.lyapunov_complex_threshold);
+  GAM =  lyapunov_symm(A,B*M_.Sigma_e*B',options_.qz_criterium,options_.lyapunov_complex_threshold,1);
+  k = find(abs(GAM) < 1e-12);
+  GAM(k) = 0;
   
+%   BB = dOm*0;
+%   for j=1:length(indx),
+%     BB(:,:,j)= dA(:,:,j)*GAM*A'+A*GAM*dA(:,:,j)'+dOm(:,:,j);
+%   end
+%   XX =  lyapunov_symm_mr(A,BB,options_.qz_criterium,options_.lyapunov_complex_threshold,0);
+  for j=1:length(indexo),
+    dum =  lyapunov_symm(A,dOm(:,:,j),options_.qz_criterium,options_.lyapunov_complex_threshold,2);
+%     dum =  XX(:,:,j);
+    k = find(abs(dum) < 1e-12);
+    dum(k) = 0;
+    dumm = vech(dum(mf,mf));
+    for i=1:nlags,
+      dum1 = A^i*dum;
+      dumm = [dumm; vec(dum1(mf,mf))];
+    end
+    JJ(:,j) = dumm;
+  end
+  nexo = length(indexo);
   for j=1:length(indx),
-    dum =  lyapunov_symm(A,dA(:,:,j)*GAM*A'+A*GAM*dA(:,:,j)'+dOm(:,:,j),options_.qz_criterium,options_.lyapunov_complex_threshold);
+    dum =  lyapunov_symm(A,dA(:,:,j+nexo)*GAM*A'+A*GAM*dA(:,:,j+nexo)'+dOm(:,:,j+nexo),options_.qz_criterium,options_.lyapunov_complex_threshold,2);
+%     dum =  XX(:,:,j);
+    k = find(abs(dum) < 1e-12);
+    dum(k) = 0;
     dumm = vech(dum(mf,mf));
     for i=1:nlags,
       dum1 = A^i*dum;
       for ii=1:i,
-        dum1 = dum1 + A^(ii-1)*dA(:,:,j)*A^(i-ii)*GAM;
+        dum1 = dum1 + A^(ii-1)*dA(:,:,j+nexo)*A^(i-ii)*GAM;
       end
       dumm = [dumm; vec(dum1(mf,mf))];
     end
-    JJ(:,j) = dumm;
+    JJ(:,j+nexo) = dumm;
   end
 end
