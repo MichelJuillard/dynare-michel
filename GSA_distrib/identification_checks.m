7cc71f613954388411dd9bbb2d7a3a437580a269
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2009-09-23 14:40:45 +0000

    First working version, with outputs: Mco, Pco, cond, ee, ind, indno
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@80 f1850c17-3b45-254b-b221-fcb05880fee1

--- GSA_distrib/identification_checks.m
+++ GSA_distrib/identification_checks.m
@@ -1,89 +1,114 @@
-function [indok, indno, indweak] = identification_checks(H,JJ)
-%In the  as2001 model, after H and J are computed,
+function [McoH, McoJ, PcoH, PcoJ, condH, condJ, eH, eJ, ind01, ind02, indnoH, indnoJ] = identification_checks(H,JJ, bayestopt_)
+
 
 % My suggestion is to have the following steps for identification check in
 % dynare:
 
 % 1. check rank of H at theta
 npar = size(H,2);
+indno = {};
+ind1 = find(vnorm(H)~=0);
+H1 = H(:,ind1);
+[e1,e2] = eig(H1'*H1);
+eH = NaN(npar,1);
+eH(ind1) = e1(:,1);
+condH = cond(H1'*H1);
+ind2 = find(vnorm(JJ)~=0);
+JJ1 = JJ(:,ind2);
+[ee1,ee2] = eig(JJ1'*JJ1);
+eJ = NaN(npar,1);
+eJ(ind2) = ee1(:,1);
+condJ = cond(JJ1'*JJ1);
+
 if rank(H)<npar
+  ixno = 0;
   %         - find out which parameters are involved,
   % using something like the vnorm and the eigenvalue decomposition of H;
-  ind1 = find(vnorm(H)~=0);
+%   disp('Some parameters are NOT identified in the model: H rank deficient')
+%   disp(' ')
   if length(ind1)<npar,
-    indno = find(~ismember([1:npar],ind1));
+    ixno = ixno + 1;
+    indnoH(ixno) = {find(~ismember([1:npar],ind1))};
+%     disp('Not identified params')
+%     disp(bayestopt_.name(indnoH{1}))
+%     disp(' ')
   end
-  [e1,e2] = eig(H'*H);
-  H1 = H(:,ind1);
-  JJ1 = JJ(:,ind1);
-
-  rank(H1)==size(H1,2)
-  rank(JJ1)==size(JJ1,2)
-
-  % to find near linear dependence problems  I use
-
-  for ii = 1:size(H1,2);
-    McoH(ii,:) = [ii,cosn([H1(:,ii),H1(:,find([1:1:size(H1,2)]~=ii))])];
-    McoJ(ii,:) = [ii,cosn([JJ1(:,ii),JJ1(:,find([1:1:size(JJ1,2)]~=ii))])];
-  end
-
-  format long  % some are nearly 1
-  McoJ
-
-
-  % here there is no exact linear dependence, but there are several
-  % near-dependencies, mostly due to strong pairwise colliniearities, which can
-  % be checked using
-
-  for ii = 1:size(H1,2);
-    for jj = 1:size(H1,2);
-      PcoH(ii,jj) = [cosn([H1(:,ii),H1(:,jj)])];
-      PcoJ(ii,jj) = [cosn([JJ1(:,ii),JJ1(:,jj)])];
-    end
+  e0 = find(abs(diag(e2))<eps);
+  for j=1:length(e0),
+    ixno = ixno + 1;
+    indnoH(ixno) = {ind1(find(e1(:,e0(j))))};
+%     disp('Perfectly collinear parameters')
+%     disp(bayestopt_.name(indnoH{ixno}))
+%     disp(' ')
   end
 else % rank(H)==length(theta), go to 2
   % 2. check rank of J
-  if rank(JJ)<size(JJ,2)
-    %         - find out which parameters are involved
-    ind1 = find(vnorm(JJ)~=0);
-    if length(ind1)<npar,
-      indno = find(~ismember([1:npar],ind1));
-    end
-    [e1,e2] = eig(J'*J);
+%   disp('All parameters are identified at theta in the model (rank of H)')
+%   disp(' ')
+end
 
-    H1 = H(:,ind1);
-    JJ1 = JJ(:,ind1);
-
-    rank(H1)==size(H1,2)
-    rank(JJ1)==size(JJ1,2)
-
-    % to find near linear dependence problems  I use
-
-    for ii = 1:size(H1,2);
-      McoH(ii,:) = [ii,cosn([H1(:,ii),H1(:,find([1:1:size(H1,2)]~=ii))])];
-      McoJ(ii,:) = [ii,cosn([JJ1(:,ii),JJ1(:,find([1:1:size(JJ1,2)]~=ii))])];
-    end
-
-    format long  % some are nearly 1
-    McoJ
-
-
-    % here there is no exact linear dependence, but there are several
-%     near-dependencies, mostly due to strong pairwise colliniearities, which can
-%     be checked using
-
-    for ii = 1:size(H1,2);
-      for jj = 1:size(H1,2);
-        PcoH(ii,jj) = [cosn([H1(:,ii),H1(:,jj)])];
-        PcoJ(ii,jj) = [cosn([JJ1(:,ii),JJ1(:,jj)])];
-      end
-    end
-  else  %rank(J)==length(theta) =>
-    disp('The parameters are identified at theta by the moments included in J')
+if rank(JJ)<npar
+  ixno = 0;
+  %         - find out which parameters are involved
+%   disp('Some parameters are NOT identified by the moments included in J')
+%   disp(' ')
+  if length(ind2)<npar,
+    ixno = ixno + 1;
+    indnoJ(ixno) = {find(~ismember([1:npar],ind2))};
   end
+  ee0 = find(abs(diag(ee2))<eps);
+  for j=1:length(ee0),
+    ixno = ixno + 1;
+    indnoJ(ixno) = {ind2(find(ee1(:,ee0(j))))};
+%     disp('Perfectly collinear parameters in moments J')
+%     disp(bayestopt_.name(indnoJ{ixno}))
+%     disp(' ')
+  end
+else  %rank(J)==length(theta) =>
+%   disp('All parameters are identified at theta by the moments included in J')
 end
 
 
+% rank(H1)==size(H1,2)
+% rank(JJ1)==size(JJ1,2)
+
+% to find near linear dependence problems  I use
+
+McoH = NaN(npar,1);
+McoJ = NaN(npar,1);
+for ii = 1:size(H1,2);
+  McoH(ind1(ii),:) = [cosn([H1(:,ii),H1(:,find([1:1:size(H1,2)]~=ii))])];
+end
+for ii = 1:size(JJ1,2);
+  McoJ(ind2(ii),:) = [cosn([JJ1(:,ii),JJ1(:,find([1:1:size(JJ1,2)]~=ii))])];
+end
+
+% format long  % some are nearly 1
+% McoJ
+
+
+% here there is no exact linear dependence, but there are several
+%     near-dependencies, mostly due to strong pairwise colliniearities, which can
+%     be checked using
+
+PcoH = NaN(npar,npar);
+PcoJ = NaN(npar,npar);
+for ii = 1:size(H1,2);
+  for jj = 1:size(H1,2);
+    PcoH(ind1(ii),ind1(jj)) = [cosn([H1(:,ii),H1(:,jj)])];
+  end
+end
+
+for ii = 1:size(JJ1,2);
+  for jj = 1:size(JJ1,2);
+    PcoJ(ind2(ii),ind2(jj)) = [cosn([JJ1(:,ii),JJ1(:,jj)])];
+  end
+end
+
+ind01 = zeros(npar,1);
+ind02 = zeros(npar,1);
+ind01(ind1) = 1;
+ind02(ind2) = 1;
 
 
 
