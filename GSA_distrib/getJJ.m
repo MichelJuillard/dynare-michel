86d7204fa4df81e4801387da111fba1ad02ab408
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2009-09-23 12:10:59 +0000

    1) Long list of output arguments 2) useautocorr
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@70 f1850c17-3b45-254b-b221-fcb05880fee1

--- GSA_distrib/getJJ.m
+++ GSA_distrib/getJJ.m
@@ -1,13 +1,17 @@
-function JJ = getJJ(M_,oo_,options_,kronflag,indx,indexo,mf,nlags)
+function [JJ, H, A, B, GAM] = getJJ(M_,oo_,options_,kronflag,indx,indexo,mf,nlags,useautocorr)
 
 if nargin<5 | isempty(indx), indx = [1:M_.param_nbr];, end,
 if nargin<6 | isempty(indexo), indexo = [];, end,
 if nargin<8 | isempty(nlags), nlags=3; end,
+if nargin<9 | isempty(useautocorr), useautocorr=0; end,
 
+  if useautocorr,
+    warning('off','MATLAB:divideByZero')
+  end
 if kronflag == -1,
   fun = 'thet2tau';
   params0 = M_.params;
-  JJ = fdjac(fun,[sqrt(diag(M_.Sigma_e(indexo,indexo))); M_.params(indx)],indx,indexo,1,mf,nlags);
+  JJ = fdjac(fun,[sqrt(diag(M_.Sigma_e(indexo,indexo))); M_.params(indx)],indx,indexo,1,mf,nlags,useautocorr);
   M_.params = params0;
   assignin('base','M_', M_);
   assignin('base','oo_', oo_);
@@ -18,6 +22,10 @@ else
   GAM =  lyapunov_symm(A,B*M_.Sigma_e*B',options_.qz_criterium,options_.lyapunov_complex_threshold,1);
   k = find(abs(GAM) < 1e-12);
   GAM(k) = 0;
+  if useautocorr,
+  sdy = sqrt(diag(GAM));
+  sy = sdy*sdy';
+  end
   
 %   BB = dOm*0;
 %   for j=1:length(indx),
@@ -29,9 +37,21 @@ else
 %     dum =  XX(:,:,j);
     k = find(abs(dum) < 1e-12);
     dum(k) = 0;
-    dumm = vech(dum(mf,mf));
+    if useautocorr
+      dsy = 1/2./sdy.*diag(dum);
+      dsy = dsy*sdy'+sdy*dsy';
+      dum1=dum;
+      dum1 = (dum1.*sy-dsy.*GAM)./(sy.*sy);
+      dum1 = dum1-diag(diag(dum1))+diag(diag(dum));
+      dumm = vech(dum1(mf,mf));
+    else
+      dumm = vech(dum(mf,mf));
+    end
     for i=1:nlags,
       dum1 = A^i*dum;
+      if useautocorr
+        dum1 = (dum1.*sy-dsy.*(A^i*GAM))./(sy.*sy);
+      end
       dumm = [dumm; vec(dum1(mf,mf))];
     end
     JJ(:,j) = dumm;
@@ -42,14 +62,34 @@ else
 %     dum =  XX(:,:,j);
     k = find(abs(dum) < 1e-12);
     dum(k) = 0;
-    dumm = vech(dum(mf,mf));
+    if useautocorr
+      dsy = 1/2./sdy.*diag(dum);
+      dsy = dsy*sdy'+sdy*dsy';
+      dum1=dum;
+      dum1 = (dum1.*sy-dsy.*GAM)./(sy.*sy);
+      dum1 = dum1-diag(diag(dum1))+diag(diag(dum));
+      dumm = vech(dum1(mf,mf));
+    else
+      dumm = vech(dum(mf,mf));
+    end
     for i=1:nlags,
       dum1 = A^i*dum;
       for ii=1:i,
         dum1 = dum1 + A^(ii-1)*dA(:,:,j+nexo)*A^(i-ii)*GAM;
       end
+      if useautocorr
+        dum1 = (dum1.*sy-dsy.*(A^i*GAM))./(sy.*sy);
+      end
       dumm = [dumm; vec(dum1(mf,mf))];
     end
     JJ(:,j+nexo) = dumm;
   end
+  
+  if nargout >4,
+    [GAM,stationary_vars] = th_autocovariances(oo_.dr,oo_.dr.order_var(mf),M_,options_);
+  end
 end
+
+  if useautocorr,
+    warning('on','MATLAB:divideByZero')
+  end
