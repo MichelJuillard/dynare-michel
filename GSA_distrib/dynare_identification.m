e045922a8ab0519f5c0106a415ae98f30088216d
rattoma <rattoma@f1850c17-3b45-254b-b221-fcb05880fee1>
2009-09-23 12:59:06 +0000

    identification engine
    git-svn-id: file:///home/sebastien/dynare/gsa_dyn@78 f1850c17-3b45-254b-b221-fcb05880fee1

--- GSA_distrib/dynare_identification.m
+++ GSA_distrib/dynare_identification.m
@@ -1,65 +1,42 @@
-In the  as2001 model, after H and J are computed,
+function dynare_identification()
 
-rank(H)==size(H,2)
-rank(JJ)==size(JJ,2)
+% main 
 
-vnorm(H)    % vnorm computes the norms of each column of the matrix. I have
-attached it below
-vnorm(JJ)
+global M_ options_ oo_ bayestopt_ estim_params_
 
-% here the problems is due to parameters 9 and 10 which affect only the
-steady state
 
-ind1 = find(vnorm(H)~=0);
-H1 = H(:,ind1);
-JJ1 = JJ(:,ind1);
+options_.mode_compute = 0;
+[data,rawdata]=dynare_estimation_init([],1);
+% computes a first linear solution to set up various variables
+dynare_resolve;
 
-rank(H1)==size(H1,2)
-rank(JJ1)==size(JJ1,2)
+options_.prior_mc=2000;
 
-% to find near linear dependence problems  I use
+SampleSize = options_.prior_mc;
 
-for ii = 1:size(H1,2);
-    McoH(ii,:) = [ii,cosn([H1(:,ii),H1(:,find([1:1:size(H1,2)]~=ii))])];
-    McoJ(ii,:) = [ii,cosn([JJ1(:,ii),JJ1(:,find([1:1:size(JJ1,2)]~=ii))])];
+% results = prior_sampler(0,M_,bayestopt_,options_,oo_);
+
+prior_draw(1,bayestopt_);
+IdentifDirectoryName = CheckPath('identification');
+
+iteration = 0;
+indx = estim_params_.param_vals(:,1);
+indexo = estim_params_.var_exo(:,1);
+useautocorr = 0;
+nlags = 3;
+
+while iteration < SampleSize,
+  loop_indx = loop_indx+1;
+  params = prior_draw();
+  set_all_parameters(params);
+
+  [JJ, H] = getJJ(M_,oo_,options_,0,indx,indexo,mf,nlags,useautocorr);  
+  
+  if ~isempty(jj),
+    iteration = iteration + 1;
+    pdraws(iteration,:) = params';
+    [indok, indno, indweak] = identification_checks(H,JJ);    
+  end
 end
 
-format long  % some are nearly 1
-McoJ
 
-
-% here there is no exact linear dependence, but there are several
-near-dependencies, mostly due to strong pairwise colliniearities, which can
-be checked using
-
-for ii = 1:size(H1,2);
-    for jj = 1:size(H1,2);
-    PcoH(ii,jj) = [cosn([H1(:,ii),H1(:,jj)])];
-    PcoJ(ii,jj) = [cosn([JJ1(:,ii),JJ1(:,jj)])];
-    end
-end
-
-
-the cosn approach is more useful for near linear dependence, i.e. weak
-identification.  For checking identification only, using the eigenvalues of
-(H'H) is easier, because it tells you immediately which columns are
-linearly dependent. For instance if 1,3 and 5 columns are linearly
-dependent, this can be discovered by  [e1,e2]= eig(H'*H);  first column of
-e1, corresponding to the 0 eigenvalue (the (1,1) % element of s2), has
-non-zero 1,3 and 5 element.   I think at this point we cannot do much
-regarding weak identifiication in dynare, except perhaps some indications
-such as small norms of the columns of H or J and strong but not perfect
-collinearity.
-
-
-My suggestion is to have the following steps for identification check in
-dynare:
-
-1. check rank of H at theta
-      if rank(H)<length(theta) - find out which parameters are involved,
-using something like the vnorm and the eigenvalue decomposition of H;
-      if rank(H)==length(theta), go to 2
-2. check rank of J
-      if rank(J)<length(theta) - find out which parameters are involved
-      if rank(J)==length(theta) => the parameters are identified at theta
-by the moments included in J
