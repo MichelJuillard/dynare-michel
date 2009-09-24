89a71d5596186cbc74a6aa7760ed71b5f534bf87
ratto <ratto@ac1d8469-bf42-47a9-8791-bf33cf982152>
2009-09-24 08:24:32 +0000

    copyright notices added
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@2966 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/cosn.m
+++ matlab/cosn.m
@@ -3,9 +3,26 @@ function co = cosn(H);
 % function co = cosn(H);
 % computes the cosine of the angle between the H(:,1) and its
 % projection onto the span of H(:,2:end)
-
+%
 % Not the same as multiple correlation coefficient since the means are not
 % zero
+%
+% Copyright (C) 2008 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 y = H(:,1);
 X = H(:,2:end);
--- matlab/getH.m
+++ matlab/getH.m
@@ -1,5 +1,23 @@
 function [H, A0, B0, dA, dOm, info] = getH(M_,oo_,kronflag,indx,indexo)
+
 % computes derivative of reduced form linear model w.r.t. deep params
+%
+% Copyright (C) 2008 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 if nargin<3 | isempty(kronflag), kronflag = 0; end
 if nargin<4 | isempty(indx), indx = [1:M_.param_nbr];, end,
