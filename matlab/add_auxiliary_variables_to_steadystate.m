bab7c9236bfc60f2186342bae6261338018f7c56
sebastien <sebastien@ac1d8469-bf42-47a9-8791-bf33cf982152>
2009-10-14 10:04:06 +0000

    Added some missing copyright headers
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@3044 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/add_auxiliary_variables_to_steadystate.m
+++ matlab/add_auxiliary_variables_to_steadystate.m
@@ -1,5 +1,24 @@
 function ys1 = add_auxiliary_variables_to_steadystate(ys,aux_vars,fname, ...
                                                       exo_steady_state, exo_det_steady_state,params)
+% Add auxiliary variables to the steady state vector
+    
+% Copyright (C) 2009 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+    
     n = length(aux_vars);
     ys1 = [ys;zeros(n,1)];
     k = size(ys,1)+1;
