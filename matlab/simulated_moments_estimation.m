1b3dce7d1b3079d35fd0fdc9be9ff82ff3946c76
stepan <stepan@ac1d8469-bf42-47a9-8791-bf33cf982152>
2010-01-06 14:38:32 +0000

    Added files for SMM estimation.
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@3309 ac1d8469-bf42-47a9-8791-bf33cf982152

---
+++ matlab/simulated_moments_estimation.m
@@ -0,0 +1,75 @@
+function [param,sigma] = simulated_moments_estimation(xparam,dataset,options)
+% Performs estimation by Simulated Moments Method.
+%
+% INPUTS:
+%  xparam          [double]  p*1 vector of initial values for the estimated parameters. 
+%  dataset         [      ]  Structure describing the data set.
+%  options         [      ]  Structure defining options for SMM.
+%
+% OUTPUTS: 
+%  param           [double]  p*1 vector of point estimates for the parameters.
+%  sigma           [double]  p*p covariance matrix of the SMM estimates.
+%
+% SPECIAL REQUIREMENTS
+%  The user has to provide a file where the moment conditions are defined.
+
+% Copyright (C) 2010 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+% Copyright (C) 2010 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.    
+
+global M_
+    
+% Load the dataset.
+eval(dataset.name);
+dataset.data = [];
+for v = 1:dataset.number_of_observed_variables
+    eval(['dataset.data = [ dataset.data , ' dataset.variables(v,:) ' ];'])
+end
+data = dataset.data(dataset.first_observation:dataset.first_observation+dataset.number_of_observations,:);
+
+% Compute sample moments and the weighting matrix.
+eval(['[sample_moments,weighting_matrix] = ' M_.fname '_moments;'])
+weighting_matrix = inv(weighting_matrix);
+
+% Initialize output.
+sigma = [];
+param = [];
+
+% Set options for csminwel. 
+H0 = 1e-4*eye(options.estimated_parameters.nb);
+ct = 1e-4;
+it = 1000;
+vb = 2;
+            
+% Call csminwel.
+[fval,param,grad,hessian_csminwel,itct,fcount,retcodehat] = ...
+                csminwel('smm_objective',xparam,H0,[],ct,it,2,sample_moments,weighting_matrix,dataset,options);
---
+++ matlab/smm_objective.m
@@ -0,0 +1,75 @@
+function [r,junk] = smm_objective(xparams,sample_moments,weighting_matrix,dataset,options)
+% Evaluates the objective of the Simulated Moments Method.
+%
+% INPUTS:
+%  xparams          [double]  p*1 vector of estimated parameters. 
+%  sample_moments   [double]  n*1 vector of sample moments (n>=p).
+%  weighting_matrix [double]  n*n symetric, positive definite matrix.
+%  dataset          [      ]  Structure describing the data set.
+%  options          [      ]  Structure defining options for SMM.
+%
+% OUTPUTS: 
+%  r                [double]  scalar, the value of the objective function.
+%  junk             [      ]  empty matrix.
+%
+% SPECIAL REQUIREMENTS
+%  The user has to provide a file where the moment conditions are defined.
+
+% Copyright (C) 2010 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.    
+
+global M_ options_
+persistent mainStream mainState
+persistent priorObjectiveValue
+    
+if isempty(mainStream)
+    mainStream = RandStream.getDefaultStream;
+    mainState  = mainStream.State;
+else
+    mainStream.State = mainState;
+end
+
+if isempty(priorObjectiveValue)
+    priorObjectiveValue = Inf;
+end
+
+junk = [];
+
+M_.params(options.estimated_parameters.idx) = xparams;
+
+% Check for local determinacy of the deterministic steady state.
+noprint = options_.noprint; options_.noprint = 1;
+[local_determinacy_and_stability,info] = check; options_.noprint = noprint;
+if ~local_determinacy_and_stability
+    r = priorObjectiveValue * (1+info(2));
+    return
+end
+
+simulated_moments = zeros(size(sample_moments));
+    
+for s = 1:options.number_of_simulated_sample
+    time_series = extended_path([],options.simulated_sample_size,1);
+    data = time_series(options.observed_variables_idx,options.burn_in_periods+1:options.burn_in_periods+dataset.number_of_observations);
+    eval(['tmp = ' options.moments_file_name '(data);'])
+    simulated_moments = simulated_moments + tmp;
+end
+
+simulated_moments = simulated_moments / options.number_of_simulated_sample;
+
+r = transpose(simulated_moments-sample_moments)*weighting_matrix*(simulated_moments-sample_moments);
+
+priorObjectiveValue = r;
