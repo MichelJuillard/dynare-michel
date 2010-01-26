234cc8ef7ab5028f277910948445cfcdc3f5e7fe
ratto <ratto@ac1d8469-bf42-47a9-8791-bf33cf982152>
2010-01-26 14:11:15 +0000

    header
    git-svn-id: https://www.dynare.org/svn/dynare/trunk@3381 ac1d8469-bf42-47a9-8791-bf33cf982152

--- matlab/myboxplot.m
+++ matlab/myboxplot.m
@@ -3,6 +3,24 @@ function sout = myboxplot (data,notched,symbol,vertical,maxwhisker)
 
 %  sout = myboxplot (data,notched,symbol,vertical,maxwhisker)
 
+%
+% Copyright (C) 2010 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
 % % % % endif
 if nargin < 5 | isempty(maxwhisker), maxwhisker = 1.5; end
 if nargin < 4 | isempty(vertical), vertical = 1; end
