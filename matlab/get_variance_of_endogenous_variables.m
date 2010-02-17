MichelJuillard/dynare-michel

commit b9173a211f618c5927964858bfa161a65ec27b38

get_variance_of_endogenous_variables.m: * fixed bug when no unit root * removed unused and never initialized second return argument

matlab/get_variance_of_endogenous_variables.m
@@ -1,6 +1,6 @@
-function [vx1,i_ns] = get_variance_of_endogenous_variables(dr,i_var)
+function vx1 = get_variance_of_endogenous_variables(dr,i_var)
 
-% function [vx1,i_ns] = get_variance_of_endogenous_variables(dr,i_var)
+% function vx1 = get_variance_of_endogenous_variables(dr,i_var)
 % Gets the variance of a variables subset
 %
 % INPUTS
@@ -9,13 +9,11 @@ function [vx1,i_ns] = get_variance_of_endogenous_variables(dr,i_var)
 %        
 % OUTPUTS
 %    vx1:       variance-covariance matrix
-%    i_ns:      non-stationary variables indices for which the variance has
-%               been calculated
 %        
 % SPECIAL REQUIREMENTS
 %    none
 
-% Copyright (C) 2003-2008 Dynare Team
+% Copyright (C) 2003-2008,2010 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -57,6 +55,7 @@ if size(u,2) > 0
     ghu = ghu(i_stat,:);
 else
     i_stat = (1:n)';
+    i_stat_0 = i_stat;
 end
 
 vx1 = Inf*ones(n,n);

commit c1e7ade63e92213cd3741085606a25ccfeb4e7e9

get_variance_of_endogenous_variables.m: fixed error introduced in commit b9173a211f618c5927964858bfa161a65ec27b38

matlab/get_variance_of_endogenous_variables.m
@@ -56,7 +56,6 @@ if size(u,2) > 0
 else
     i_stat_0 = 1:size(ghx,2);
     i_stat = (1:n)';
-    i_stat_0 = i_stat;
 end
 
 vx1 = Inf*ones(n,n);