1dd9ca8e3fd29d2bb0d82915d2e729345cd02e3f
Sébastien Villemot <sebastien.villemot@ens.fr>
2010-03-29 12:44:56 +0200

    Activate warning backtrace even for Octave

--- matlab/warning_config.m
+++ matlab/warning_config.m
@@ -10,7 +10,7 @@ function warning_config()
 % SPECIAL REQUIREMENTS
 %   none
 
-% Copyright (C) 2008 Dynare Team
+% Copyright (C) 2008-2010 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -28,6 +28,10 @@ function warning_config()
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 warning on;
+
+% Display a calling stack trace when a warning is issued
+warning('on', 'backtrace');
+
 if exist('OCTAVE_VERSION')
     warning('off', 'Octave:separator-insert');
     warning('off', 'Octave:matlab-incompatible');
@@ -40,6 +44,4 @@ if exist('OCTAVE_VERSION')
     warning('off', 'Octave:string-concat');
     warning('off', 'Octave:variable-switch-label');
     warning('off', 'Octave:fortran-indexing');
-else
-    warning backtrace;
 end
