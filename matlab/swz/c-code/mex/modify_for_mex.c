358640ba4f5a36a139cc06af107fde302097ade0
Houtan Bastani <houtanb@gmail.com>
2010-05-19 15:55:47 +0200

    Mex files / modifications for mex

--- matlab/swz/c-code/mex/modify_for_mex.c
+++ matlab/swz/c-code/mex/modify_for_mex.c
@@ -1,12 +1,36 @@
-#include "mex.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdarg.h>
 #include <string.h>
 
+#if defined(MATLAB_MEX_FILE) || defined(OCTAVE_MEX_FILE)
+#include <dynmex.h>
+#endif
+
+int
+swz_fprintf_stdout(char *msg, ...)
+{
+  int ret;
+  va_list ap;
+  va_start(ap, msg);
+
+#if defined(MATLAB_MEX_FILE)
+  ret = mexPrintf(msg, ap);
+#elif defined(OCTAVE_MEX_FILE)
+  mexPrintf(msg, ap);
+  ret = 1;
+#else
+  ret = vprintf(msg, ap);
+#endif
+
+  va_end(ap);
+  return ret;
+}
+
 void
-swz_fprintf_err(FILE *cad, const char * str, ...)
+swz_fprintf_err(char *str, ...)
 {
   char *whole_str=(char*)NULL;
   char *msg_truncated = ".....MSG TRUNCATED\n";
@@ -16,30 +40,7 @@ swz_fprintf_err(FILE *cad, const char * str, ...)
   va_start(ap, str);
 
 #if defined(MATLAB_MEX_FILE) || defined(OCTAVE_MEX_FILE)
-  if (!(whole_str = (char *)malloc(sizeof(str)*sizeof(char))))
-    {
-      printf("Could not allocate memory\n");
-      exit(0);
-    }
-
-  strcpy(whole_str, str);
-  while (strtok_r(whole_str, "%", &whole_str) != NULL)
-    num_args++;
-
-  num_args = sizeof(str)*sizeof(char) + num_args*sizeof(long double);
-  if (!(whole_str = (char *)realloc(whole_str, num_args + strlen(msg_truncated) + 1)))
-    {
-      printf("Could not allocate memory\n");
-      exit(0);
-    }
-
-  vsnprintf(whole_str, num_args, str, ap);
-  if(strlen(whole_str) + 1 == num_args)
-    strcat(whole_str, msg_truncated);
-
-  printf("%s", whole_str);
-
-  free(whole_str);
+  mexPrintf(str, ap);
 #else
   vfprintf(stderr, str, ap);
 #endif
--- matlab/swz/c-code/mex/modify_for_mex.h
+++ matlab/swz/c-code/mex/modify_for_mex.h
@@ -2,9 +2,31 @@
 #define _MEXMOD
 void swz_exit(int status);
 void swz_fprintf_err(const char * str, ...);
+int swz_fprintf_stdout(char *msg, ...);
 #endif
 
+
 #if defined(MATLAB_MEX_FILE) || defined(OCTAVE_MEX_FILE)
+#include "matrix.h"
+#include <dynmex.h>
+
+/*  //#undef printf   ansi-c*/
+/*  //#define printf swz_printf   ansi-c*/
+/*#define fflush(stdout) mexEvalString("drawnow;");*/
+
+#undef printf
+#undef exit
+
 #define printf mexPrintf
 #define exit swz_exit
+
+#undef malloc
+#undef calloc
+#undef realloc
+#undef free
+
+#define malloc mxMalloc
+#define calloc mxCalloc
+#define realloc mxRealloc
+#define free mxFree
 #endif
