160922a1899e759ab1da574b78753cd69d445797
Stéphane Adjemian (Charybdis) <stephane.adjemian@ens.fr>
2010-11-19 14:39:54 +0100

    Added mex file for computing the fixed point of the riccati equation in the kalman filter. The mex works with octave but not with matlab (lapack issue again ;-).

---
+++ tests/kalman_steady_state/test1.m
@@ -0,0 +1,38 @@
+n = 10;
+p = 3;
+q = 4;
+
+% With measurment errors.
+for i=1:100
+    tmp = randn(q,q+100);
+    Q = tmp*transpose(tmp)/1000;
+    R = randn(n,q);
+    QQ = R*Q*transpose(R);
+    tmp = randn(p,p+100);
+    H = tmp*transpose(tmp)/2000;
+    eig_val_T = rand(n)*2-1;
+    eig_vec_T = randn(n,n);
+    T = eig_vec_T*eig_val_T*inv(eig_vec_T);
+    Z = zeros(p,n);
+    Z(1,1) = 1;
+    Z(2,3) = 1;
+    Z(3,6) = 1;
+    P = kalman_steady_state(transpose(T),QQ,transpose(Z),H);
+end
+
+% Without measurment errors.
+for i=1:100
+    tmp = randn(q,q+100);
+    Q = tmp*transpose(tmp)/1000;
+    R = randn(n,q);
+    QQ = R*Q*transpose(R);
+    H = zeros(p,p);
+    eig_val_T = rand(n)*2-1;
+    eig_vec_T = randn(n,n);
+    T = eig_vec_T*eig_val_T*inv(eig_vec_T);
+    Z = zeros(p,n);
+    Z(1,1) = 1;
+    Z(2,3) = 1;
+    Z(3,6) = 1;
+    P = kalman_steady_state(transpose(T),QQ,transpose(Z),H);
+end
