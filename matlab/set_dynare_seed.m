116bf69fbf7204ca3c06a74d27942e4b41060999
Sébastien Villemot <sebastien.villemot@ens.fr>
2010-11-29 11:23:59 +0100

    Seed: fix bug under Octave and MATLAB < 7.7

--- matlab/set_dynare_seed.m
+++ matlab/set_dynare_seed.m
@@ -24,10 +24,7 @@ if ~nargin
     error('set_dynare_seed:: I need at least one input argument!')
 end
 
-matlab_random_streams = 1;
-if  exist('OCTAVE_VERSION') || matlab_ver_less_than('7.7')
-    matlab_random_streams = 1;
-end
+matlab_random_streams = ~(exist('OCTAVE_VERSION') || matlab_ver_less_than('7.7'));
 
 if matlab_random_streams% Use new matlab interface.
     if nargin==1
