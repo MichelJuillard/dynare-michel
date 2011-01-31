37efe120fae6282935c14d37bbe81fcb0794f5d9
Ferhat Mihoubi <ferhat.mihoubi@univ-evry.fr>
2011-01-31 09:58:13 +0100

    Allows for block decomposition if "simul" command is not present in the mod file.

--- preprocessor/ModFile.cc
+++ preprocessor/ModFile.cc
@@ -324,7 +324,7 @@ ModFile::computingPass(bool no_tmp_terms)
               bool hessian = mod_file_struct.order_option >= 2 || mod_file_struct.identification_present;
               bool thirdDerivatives = mod_file_struct.order_option == 3;
               bool paramsDerivatives = mod_file_struct.identification_present;
-              dynamic_model.computingPass(true, hessian, thirdDerivatives, paramsDerivatives, global_eval_context, no_tmp_terms, false, use_dll, byte_code);
+              dynamic_model.computingPass(true, hessian, thirdDerivatives, paramsDerivatives, global_eval_context, no_tmp_terms, block, use_dll, byte_code);
             }
         }
       else
