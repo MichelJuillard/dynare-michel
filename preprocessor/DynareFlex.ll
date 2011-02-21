MichelJuillard/dynare-michel

commit 412e8a7da0f1ceed51a5c8d88c27a9733b4daccb

Preprocessor: fix problem with "%" sign inside string in native statements

preprocessor/DynareFlex.ll
@@ -580,7 +580,9 @@ string eofbuff;
 
  /* Add the native statement */
 <NATIVE>{
-  [^/%*\n\.]*                 |
+  [^/%*\n\.\'\"]*             |
+  \'[^\'\n]*\'                |
+  \"[^\"\n]*\"                |
   \.{1,2}                     |
   "*"                         |
   "/"                         { yymore(); eofbuff = string(yytext); }

tests/comments.mod
@@ -28,6 +28,8 @@ fff = [ 1 delta 4 ; ...       /* COMMENT
      1     0 4 ; ...     // comment */
          phi 9 4 ]        % comment
 
+disp(' %% This is not a comment %% ')
+
 model;
 c*theta*h^(1+psi)=(1-alpha)*y;
 k = beta*(((exp(b)*c)/(exp(b(+1))*c(+1)))

commit 056dfeb1696f016b91f112d65599c08ef7c2caf8

Preprocessor: fix bug introduced in 412e8a7da0f1ceed51a5c8d88c27a9733b4daccb The transpose sign (single quote) was no longer recognized in native statements

preprocessor/DynareFlex.ll
@@ -581,6 +581,7 @@ string eofbuff;
  /* Add the native statement */
 <NATIVE>{
   [^/%*\n\.\'\"]*             |
+  \'                          |
   \'[^\'\n]*\'                |
   \"[^\"\n]*\"                |
   \.{1,2}                     |