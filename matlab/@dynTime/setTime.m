5ee5b1a8f0084fd73721372f9e16f894bbdcff22
Stéphane Adjemian (Scylla) <stephane.adjemian@univ-lemans.fr>
2011-09-09 15:56:58 +0200

    Added an alternative calling sequence (with two inputs instead of three).

--- matlab/@dynTime/setTime.m
+++ matlab/@dynTime/setTime.m
@@ -1,2 +1,10 @@
 function sp = setTime(sp,i,date)
-    sp.time(i,:) = date;
+    if nargin==3
+        sp.time(i,:) = date;
+    elseif nargin==2
+        if isa(i,'dynTime')
+            sp.time=i.time;
+        else
+            sp.time=i;
+        end
+    end
