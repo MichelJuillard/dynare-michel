f4f46dd82b855074e63bae90e593394f19e45f73
Houtan Bastani <houtan.bastani@ens.fr>
2011-10-06 15:16:03 +0200

    MS-SBVAR: update copyrights

--- matlab/ms-sbvar/identification/lower_cholesky.m
+++ matlab/ms-sbvar/identification/lower_cholesky.m
@@ -1,4 +1,37 @@
 function [Ui,Vi,n0,np,ixmC0Pres] = lower_cholesky(nvar,nexo,options_ms)
+% function [Ui,Vi,n0,np,ixmC0Pres] = lower_cholesky(nvar,nexo,options_ms)
+%
+% INPUTS
+%    nvar:                      number endogenous variables
+%    nexo:                      number exogenous variables
+%    options_ms:    (struct)    options
+%
+% OUTPUTS
+%    Ui
+%    Vi
+%    n0
+%    np
+%    ixmC0Pres
+%
+% SPECIAL REQUIREMENTS
+%    none
+
+% Copyright (C) 2011 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 lags = options_ms.nlags;
 indxC0Pres = options_ms.cross_restrictions;
