117996c881016d966d1d08bfe08d96803f4bb3f4
Michel Juillard <michel.juillard@mjui.fr>
2011-10-20 20:39:00 +0200

    code simplification after commit 9425b36358b9b1a6dcfb82cbf0cd423c64748bff

--- preprocessor/DynamicModel.cc
+++ preprocessor/DynamicModel.cc
@@ -4019,10 +4019,7 @@ DynamicModel::substituteLeadLagInternal(aux_var_t type, bool deterministic_model
   //  - expectation (entered before this function is called)
   //  - lead variables from lower lead to higher lead (need to be listed in reverse order)
   //  - lag variables from lower lag to higher lag
-  if ((type == avEndoLead) || (type == avExoLead))
-    copy(neweqs.begin(), neweqs.end(), back_inserter(aux_equations));
-  else
-    copy(neweqs.begin(), neweqs.end(), back_inserter(aux_equations));
+  copy(neweqs.begin(), neweqs.end(), back_inserter(aux_equations));
 
   if (neweqs.size() > 0)
     {
