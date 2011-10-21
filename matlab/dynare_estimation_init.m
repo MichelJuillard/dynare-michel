7e3225680fa7dfc9a2c28c5bcf8ffb4d127cab98
Michel Juillard <michel.juillard@ens.fr>
2011-10-21 14:21:40 +0200

    fixing logic flaw in setting lik_init as a function of diffuse filter or kalman_algo

--- matlab/dynare_estimation_init.m
+++ matlab/dynare_estimation_init.m
@@ -66,8 +66,9 @@ if options_.order > 1 & isempty(options_.nonlinear_filter)
     options_.order = 1;
 end
 
-% Set options_.lik_init equal to 3 if diffuse filter is used.
-if (options_.diffuse_filter==1) && (options_.lik_init==1)
+% Set options_.lik_init equal to 3 if diffuse filter is used or
+% kalman_algo refers to a diffuse filter algorithm.
+if (options_.diffuse_filter==1) || (options_.kalman_algo > 2)
     options_.lik_init = 3;
 end
 
