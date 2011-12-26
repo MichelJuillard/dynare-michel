300eaf63c2e4e70346a38d7489d4f75364f72557
Sébastien Villemot <sebastien.villemot@ens.fr>
2011-12-26 15:39:54 +0100

    Remove nonfree LPTAU.m, replace by qmc_sequence MEX

--- matlab/gsa/lptauSEQ.m
+++ matlab/gsa/lptauSEQ.m
@@ -2,7 +2,7 @@ function [lpmat] = lptauSEQ(Nsam,Nvar)
 
 % [lpmat] = lptauSEQ(Nsam,Nvar)
 %
-% function call LPTAU and generates a sample of dimension Nsam for a
+% Generates a sample from a sobol sequence of length Nsam for a
 % number of parameters Nvar
 %
 % Copyright (C) 2005 Marco Ratto
@@ -19,7 +19,9 @@ function [lpmat] = lptauSEQ(Nsam,Nvar)
 
 clear lptau
 lpmat = zeros(Nsam, Nvar);
+seed = int64(0);
 for j=1:Nsam,
-    lpmat(j,:)=LPTAU(j,Nvar);
+    [v, seed] = qmc_sequence(Nvar, seed, 0);
+    lpmat(j,:) = v;
 end
 return
