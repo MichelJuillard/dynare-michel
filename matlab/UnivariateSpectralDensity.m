MichelJuillard/dynare-michel

commit bda0b1b228f4e1f3ddd92ed3c24de8d1663ce406

Move hardcoded options from UnivariateSpectralDensity.m to global_initialization.

matlab/UnivariateSpectralDensity.m
@@ -34,9 +34,9 @@ if options_.order > 1
     return
 end
 
-pltinfo  = 1;%options_.SpectralDensity.Th.plot;
-cutoff   = 150;%options_.SpectralDensity.Th.cutoff;
-sdl      = 0.01;%options_.SepctralDensity.Th.sdl;
+pltinfo  = options_.SpectralDensity.plot;
+cutoff   = options_.SpectralDensity.cutoff;
+sdl      = options_.SepctralDensity.sdl;
 omega    = (0:sdl:pi)';
 GridSize = length(omega);
 exo_names_orig_ord  = M_.exo_names_orig_ord;

matlab/global_initialization.m
@@ -126,6 +126,10 @@ options_.nocorr = 0;
 options_.periods = 0;
 options_.noprint = 0;
 options_.SpectralDensity = 0;
+options_.SpectralDensity.plot  = 1;
+options_.SpectralDensity.cutoff  = 150;
+options_.SpectralDensity.sdl = 0.01;
+
 
 % Extended path options
 %

commit c0624738c987b2c818ddad65835f99e2d44aad6f

Fixed bug introduced in bda0b1b228f4e1f3ddd92ed3c24de8d1663ce406 Created separate field options_.SpectralDensity.trigger to trigger Spectral Density

matlab/UnivariateSpectralDensity.m
@@ -33,10 +33,9 @@ if options_.order > 1
     disp('Set order = 1.')
     return
 end
-
-pltinfo  = 1;%options_.SpectralDensity.Th.plot;
-cutoff   = 150;%options_.SpectralDensity.Th.cutoff;
-sdl      = 0.01;%options_.SepctralDensity.Th.sdl;
+pltinfo  = options_.SpectralDensity.plot; 
+cutoff   = options_.SpectralDensity.cutoff; 
+sdl      = options_.SepctralDensity.sdl; 
 omega    = (0:sdl:pi)';
 GridSize = length(omega);
 exo_names_orig_ord  = M_.exo_names_orig_ord;

matlab/global_initialization.m
@@ -125,7 +125,10 @@ options_.nomoments = 0;
 options_.nocorr = 0;
 options_.periods = 0;
 options_.noprint = 0;
-options_.SpectralDensity = 0;
+options_.SpectralDensity.trigger = 0;
+options_.SpectralDensity.plot  = 1; 
+options_.SpectralDensity.cutoff  = 150; 
+options_.SpectralDensity.sdl = 0.01; 
 
 % Extended path options
 %

matlab/stoch_simul.m
@@ -323,7 +323,7 @@ if options_.irf
     end
 end
 
-if options_.SpectralDensity == 1
+if options_.SpectralDensity.trigger == 1
     [omega,f] = UnivariateSpectralDensity(oo_.dr,var_list);
 end
 

matlab/stoch_simul_sparse.m
@@ -286,7 +286,7 @@ if options_.irf
     end
 end
 
-if options_.SpectralDensity == 1
+if options_.SpectralDensity.trigger == 1
     [omega,f] = UnivariateSpectralDensity(oo_.dr,var_list);
 end
 