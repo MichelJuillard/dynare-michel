MichelJuillard/dynare-michel

commit 4c255cf2ae7bd8001ab2fe5290b20c3233cdb6ec

Starts implementing Ticket 216. Most plotting and figure is now controlled by the functions dyn_figure and dyn_saveas, allowing for option_.nodisplay to suppress the displaying of figures and using options_.graph_format to control the format of saved figures.

matlab/McMCDiagnostics.m
@@ -130,11 +130,7 @@ clear pmet temp moyenne CSUP CINF csup cinf n linea iter tmp;
 pages = floor(npar/3);
 k = 0;  
 for i = 1:pages
-    if options_.nograph
-        h = figure('Name','MCMC univariate diagnostic (Brooks and Gelman,1998)','Visible','off');
-    else
-        h = figure('Name','MCMC univariate diagnostic (Brooks and Gelman,1998)');
-    end    
+    h=dyn_figure(options_,'Name','MCMC univariate diagnostic (Brooks and Gelman,1998)');
     boxplot = 1;
     for j = 1:3 % Loop over parameters
         k = k+1;
@@ -172,15 +168,7 @@ for i = 1:pages
             boxplot = boxplot + 1;
         end
     end
-    eval(['print -depsc2 ' DirectoryName '/' M_.fname '_udiag' int2str(i) '.eps']);
-    if ~exist('OCTAVE_VERSION')
-        eval(['print -dpdf ' DirectoryName '/' M_.fname '_udiag' int2str(i)]);
-    end
-    if options_.nograph, set(h,'visible','on'), end
-    if ~exist('OCTAVE_VERSION')
-        saveas(h,[DirectoryName '/' M_.fname '_udiag' int2str(i) '.fig']);
-    end
-    if options_.nograph, close(h), end
+    dyn_saveas(h,[DirectoryName '/' M_.fname '_udiag' int2str(i)],options_);
     if TeX
         fprintf(fidTeX,'\\begin{figure}[H]\n');
         for jj = 1:size(NAMES,1)
@@ -205,10 +193,7 @@ if reste
         nr = 2;
         nc = 3;
     end
-    if options_.nograph
-        h = figure('Name','MCMC univariate diagnostic (Brooks and Gelman, 1998)','Visible','off');
-    else
-        h = figure('Name','MCMC univariate diagnostic (Brooks and Gelman, 1998)');
+    h = dyn_figure(options_,'Name','MCMC univariate diagnostic (Brooks and Gelman, 1998)');
     end  
     boxplot = 1;
     for j = 1:reste
@@ -247,15 +232,7 @@ if reste
             boxplot = boxplot + 1;
         end
     end
-    eval(['print -depsc2 ' DirectoryName '/' M_.fname '_udiag' int2str(pages+1) '.eps']);
-    if ~exist('OCTAVE_VERSION')
-        eval(['print -dpdf ' DirectoryName '/' M_.fname '_udiag' int2str(pages+1)]);
-    end
-    if options_.nograph, set(h,'visible','on'), end
-    if ~exist('OCTAVE_VERSION')
-        saveas(h,[DirectoryName '/' M_.fname '_udiag' int2str(pages+1) '.fig']);
-    end
-    if options_.nograph, close(h), end
+    dyn_saveas(h,[ DirectoryName '/' M_.fname '_udiag' int2str(pages+1)],options_);
     if TeX
         fprintf(fidTeX,'\\begin{figure}[H]\n');
         for jj = 1:size(NAMES,1);
@@ -331,11 +308,8 @@ for iter  = Origin:StepSize:NumberOfDraws
     end
 end
 MDIAG(:,[2 4 6],:) = MDIAG(:,[2 4 6],:)/nblck;  
-if options_.nograph
-    h = figure('Name','Multivariate diagnostic','Visible','off');
-else
-    h = figure('Name','Multivariate diagnostic');
-end
+
+h = dyn_figure(options_,'Name','Multivariate diagnostic');
 boxplot = 1;
 for crit = 1:3
     if crit == 1
@@ -367,15 +341,8 @@ for crit = 1:3
     title(namnam,'Interpreter','none');
     boxplot = boxplot + 1;
 end
-eval(['print -depsc2 ' DirectoryName '/' M_.fname '_mdiag.eps']);
-if ~exist('OCTAVE_VERSION')
-    eval(['print -dpdf ' DirectoryName '/' M_.fname '_mdiag']);
-end
-if options_.nograph, set(h,'visible','on'), end
-if ~exist('OCTAVE_VERSION')
-    saveas(h,[DirectoryName '/' M_.fname '_mdiag.fig']);
-end
-if options_.nograph, close(h), end
+dyn_saveas(h,[ DirectoryName '/' M_.fname '_mdiag'],options_);
+
 if TeX
     fprintf(fidTeX,'\\begin{figure}[H]\n');
     for jj = 1:3

matlab/gsa/filt_mc_.m
@@ -289,7 +289,7 @@ else
     for i=1:size(vvarvecm,1),
         if mod(i,9)==1,
             ifig=ifig+1;
-            figure('name',['Prior ',int2str(ifig)])
+            hh=dyn_figure(options_,'name',['Prior ',int2str(ifig)]);
         end
         subplot(3,3,i-9*(ifig-1))
         h=cumplot(lnprior(ixx(1:nfilt0(i),i)));
@@ -300,34 +300,24 @@ else
         title(vvarvecm(i,:),'interpreter','none')
         if mod(i,9)==0 | i==size(vvarvecm,1)
             if options_.opt_gsa.ppost
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_post_lnprior',int2str(ifig)  '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_post_lnprior',int2str(ifig)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_post_lnprior',int2str(ifig)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_post_lnprior',int2str(ifig)],options_);
             else
                 if options_.opt_gsa.pprior
-                    eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_prior_lnprior',int2str(ifig)  '.eps']);
-                    if ~exist('OCTAVE_VERSION'),
-                        saveas(gcf,[OutDir,'/',fname_,'_rmse_prior_lnprior',int2str(ifig)])
-                        eval(['print -dpdf ' OutDir '/' fname_ '_rmse_prior_lnprior',int2str(ifig)]);
-                    end
+                    dyn_saveas(hh,[OutDir '/' fname_ '_rmse_prior_lnprior',int2str(ifig) ],options_);
                 else
-                    eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_mc_lnprior',int2str(ifig)  '.eps']);
-                    if ~exist('OCTAVE_VERSION'),
-                        saveas(gcf,[OutDir,'/',fname_,'_rmse_mc_lnprior',int2str(ifig)])
-                        eval(['print -dpdf ' OutDir '/' fname_ '_rmse_mc_lnprior',int2str(ifig)]);
-                    end
+                    dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_lnprior',int2str(ifig) ],options_);
                 end
             end
-            close(gcf)
+            if ~options.nodisplay
+                close(hh);
+            end
         end
     end
     ifig=0;
     for i=1:size(vvarvecm,1),
         if mod(i,9)==1,
             ifig=ifig+1;
-            figure('name',['Likelihood ',int2str(ifig)])
+            hh = dyn_figure(options_,'Name',['Likelihood ',int2str(ifig)]);
         end
         subplot(3,3,i-9*(ifig-1))
         h=cumplot(likelihood(ixx(1:nfilt0(i),i)));
@@ -341,34 +331,24 @@ else
         end
         if mod(i,9)==0 | i==size(vvarvecm,1)
             if options_.opt_gsa.ppost
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_post_lnlik',int2str(ifig)  '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_post_lnlik',int2str(ifig)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_post_lnlik',int2str(ifig)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_post_lnlik',int2str(ifig) ],options_);
             else
                 if options_.opt_gsa.pprior
-                    eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_prior_lnlik',int2str(ifig)  '.eps']);
-                    if ~exist('OCTAVE_VERSION'),
-                        saveas(gcf,[OutDir,'/',fname_,'_rmse_prior_lnlik',int2str(ifig)])
-                        eval(['print -dpdf ' OutDir '/' fname_ '_rmse_prior_lnlik',int2str(ifig)]);
-                    end
+                    dyn_saveas(hh,[OutDir '/' fname_ '_rmse_prior_lnlik',int2str(ifig)],options_);
                 else
-                    eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_mc_lnlik',int2str(ifig)  '.eps']);
-                    if ~exist('OCTAVE_VERSION'),
-                        saveas(gcf,[OutDir,'/',fname_,'_rmse_mc_lnlik',int2str(ifig)])
-                        eval(['print -dpdf ' OutDir '/' fname_ '_rmse_mc_lnlik',int2str(ifig)]);
-                    end
+                    dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_lnlik',int2str(ifig) ],options_);
                 end
             end
-            close(gcf)
+            if ~options.nodisplay
+                close(hh);
+            end
         end
     end
     ifig=0;
     for i=1:size(vvarvecm,1),
         if mod(i,9)==1,
             ifig=ifig+1;
-            figure('name',['Posterior ',int2str(ifig)])
+            hh = dyn_figure(options_,'Name',['Posterior ',int2str(ifig)]);
         end
         subplot(3,3,i-9*(ifig-1))
         h=cumplot(logpo2(ixx(1:nfilt0(i),i)));
@@ -382,27 +362,17 @@ else
         end
         if mod(i,9)==0 | i==size(vvarvecm,1)
             if options_.opt_gsa.ppost
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_post_lnpost',int2str(ifig)  '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_post_lnpost',int2str(ifig)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_post_lnpost',int2str(ifig)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_post_lnpost',int2str(ifig) ],options_);
             else
                 if options_.opt_gsa.pprior
-                    eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_prior_lnpost',int2str(ifig)  '.eps']);
-                    if ~exist('OCTAVE_VERSION'),
-                        saveas(gcf,[OutDir,'/',fname_,'_rmse_prior_lnpost',int2str(ifig)])
-                        eval(['print -dpdf ' OutDir '/' fname_ '_rmse_prior_lnpost',int2str(ifig)]);
-                    end
+                    dyn_saveas(hh,[OutDir '/' fname_ '_rmse_prior_lnpost',int2str(ifig)],options_);
                 else
-                    eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_mc_lnpost',int2str(ifig)  '.eps']);
-                    if ~exist('OCTAVE_VERSION'),
-                        saveas(gcf,[OutDir,'/',fname_,'_rmse_mc_lnpost',int2str(ifig)])
-                        eval(['print -dpdf ' OutDir '/' fname_ '_rmse_mc_lnpost',int2str(ifig)]);
-                    end
+                    dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_lnpost',int2str(ifig)],options_);
                 end
             end
-            close(gcf)
+            if ~options.nodisplay
+                close(hh);
+            end
         end
     end
     
@@ -501,7 +471,7 @@ else
     % plot trade-offs
     a00=jet(size(vvarvecm,1));
     for ix=1:ceil(length(nsnam)/5),
-        figure,
+        hh = dyn_figure(options_);
         for j=1+5*(ix-1):min(size(snam2,1),5*ix),
             subplot(2,3,j-5*(ix-1))
             %h0=cumplot(x(:,nsnam(j)+nshock));
@@ -536,24 +506,12 @@ else
         %h0=legend({'base',vnam{np}}',0);
         %set(findobj(get(h0,'children'),'type','text'),'interpreter','none')
         if options_.opt_gsa.ppost
-            eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_post_' int2str(ix) '.eps']);
-            if ~exist('OCTAVE_VERSION'),
-                saveas(gcf,[OutDir,'/',fname_,'_rmse_post_',num2str(ix)])
-                eval(['print -dpdf ' OutDir '/' fname_ '_rmse_post_' int2str(ix)]);
-            end
+            dyn_saveas(hh,[ OutDir '/' fname_ '_rmse_post_' int2str(ix)],options_);
         else
             if options_.opt_gsa.pprior
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_prior_' int2str(ix) '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_prior_',num2str(ix)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_prior_' int2str(ix)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_prior_' int2str(ix) ],options_);
             else
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_mc_' int2str(ix)  '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_mc_',num2str(ix)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_mc_' int2str(ix)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_' int2str(ix)],options_);
             end
         end
     end
@@ -569,7 +527,7 @@ else
     %kernel_function = 'uniform';     % Gaussian kernel for Fast Fourrier Transform approximaton.
     
     for ix=1:ceil(length(nsnam)/5),
-        figure,
+        hh = dyn_figure(options_);
         for j=1+5*(ix-1):min(size(snam2,1),5*ix),
             subplot(2,3,j-5*(ix-1))
             optimal_bandwidth = mh_optimal_bandwidth(x(:,nsnam(j)),size(x,1),bandwidth,kernel_function);
@@ -607,29 +565,16 @@ else
         %h0=legend({'base',vnam{np}}',0);
         %set(findobj(get(h0,'children'),'type','text'),'interpreter','none')
         if options_.opt_gsa.ppost
-            eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_post_dens_' int2str(ix)  '.eps']);
-            if ~exist('OCTAVE_VERSION'),
-                saveas(gcf,[OutDir,'/',fname_,'_rmse_post_dens_',num2str(ix)])
-                eval(['print -dpdf ' OutDir '/' fname_ '_rmse_post_dens_' int2str(ix)]);
-            end
+            dyn_saveas(hh,[ OutDir '/' fname_ '_rmse_post_dens_' int2str(ix) ],options_);
         else
             if options_.opt_gsa.pprior
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_prior_dens_' int2str(ix)  '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_prior_dens_',num2str(ix)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_prior_dens_' int2str(ix)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_prior_dens_' int2str(ix)],options_);
             else
-                eval(['print -depsc2 ' OutDir '/' fname_ '_rmse_mc_dens_' int2str(ix)  '.eps']);
-                if ~exist('OCTAVE_VERSION'),
-                    saveas(gcf,[OutDir,'/',fname_,'_rmse_mc_dens_',num2str(ix)])
-                    eval(['print -dpdf ' OutDir '/' fname_ '_rmse_mc_dens_' int2str(ix)]);
-                end
+                dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_dens_' int2str(ix) ],options_);
             end
         end
     end
     close all
-    
     % for j=1:size(SP,2),
     %     nfig=0;
     %     np=find(SP(:,j));

matlab/gsa/map_ident_.m
@@ -77,7 +77,7 @@ if opt_gsa.load_ident_files==0,
     ifig=0;
     for j=1:M_.exo_nbr,
       if mod(j,6)==1
-        figure('name',['Variance decomposition shocks']);
+        hh=dyn_figure(options_,'name',['Variance decomposition shocks']);
         ifig=ifig+1;
         iplo=0;
       end
@@ -95,10 +95,10 @@ if opt_gsa.load_ident_files==0,
       ylabel(' ')
       title(M_.exo_names(j,:),'interpreter','none')
       if mod(j,6)==0 | j==M_.exo_nbr,
-        saveas(gcf,[OutputDirectoryName,'/',fname_,'_vdec_exo_',int2str(ifig)])
-        eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_vdec_exo_',int2str(ifig)]);
-        eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_vdec_exo_',int2str(ifig)]);
-        close(gcf),
+        dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_vdec_exo_',int2str(ifig)],options_);  
+        if ~options.nodisplay
+            close(hh);
+        end      
       end
     end
   end
@@ -204,7 +204,7 @@ if opt_gsa.morris==1,
     load([OutputDirectoryName,'/',fname_,'_morris_IDE'],'SAvdec','vdec','ir_vdec','ic_vdec')
   end
   
-  figure,
+  hh = dyn_figure(options_);
 %   boxplot(SAvdec,'whis',10,'symbol','r.')
   myboxplot(SAvdec,[],'.',[],10)
   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:npT])
@@ -217,10 +217,10 @@ if opt_gsa.morris==1,
   end
   xlabel(' ')
   title('All variance decomposition')
-  saveas(gcf,[OutputDirectoryName,'/',fname_,'_morris_vdec'])
-  eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_morris_vdec']);
-  eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_morris_vdec']);
-  close(gcf)
+  dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morris_vdec'],options_);
+  if ~options.nodisplay
+    close(hh);
+  end
   else
   save([OutputDirectoryName,'/',fname_,'_morris_IDE'],'vdec')
     
@@ -312,7 +312,7 @@ if opt_gsa.morris==1,
     load([OutputDirectoryName,'/',fname_,'_morris_IDE'],'ac','ir_ac','ic_ac')
   end
   
-  figure,
+  hh=dyn_figure(options_);
 %   boxplot(SAcc,'whis',10,'symbol','r.')
   myboxplot(SAcc,[],'.',[],10)
   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:npT])
@@ -325,9 +325,7 @@ if opt_gsa.morris==1,
   end
   xlabel(' ')
   title('EET All moments')
-  saveas(gcf,[OutputDirectoryName,'/',fname_,'_morris_moments'])
-  eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_morris_moments']);
-  eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_morris_moments']);
+  dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morris_moments'],options_);
 %   close(gcf),
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -709,7 +707,7 @@ if opt_gsa.morris==1,
   else
     load([OutputDirectoryName,'/',fname_,'_morris_IDE'],'SAnorm','SAmunorm','SAsignorm')
   end
-  figure, %bar(SAnorm(:,irel))
+  hh=dyn_figure(options_); %bar(SAnorm(:,irel))
 %   boxplot(SAnorm','whis',10,'symbol','r.')
   myboxplot(SAnorm',[],'.',[],10)
   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:npT])
@@ -723,11 +721,9 @@ if opt_gsa.morris==1,
   end
   xlabel(' ')
   title('Elementary effects in the model')
-  saveas(gcf,[OutputDirectoryName,'/',fname_,'_morris_par'])
-  eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_morris_par']);
-  eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_morris_par']);
+  dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morris_par'],options_);
 
-  figure, %bar(SAmunorm(:,irel))
+  hh=dyn_figure(options_); %bar(SAmunorm(:,irel))
 %   boxplot(SAmunorm','whis',10,'symbol','r.')
   myboxplot(SAmunorm',[],'.',[],10)
   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:npT])
@@ -740,12 +736,12 @@ if opt_gsa.morris==1,
   end
   xlabel(' ')
   title('\mu in the model')
-  saveas(gcf,[OutputDirectoryName,'/',fname_,'_morrismu_par'])
-  eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_morrismu_par']);
-  eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_morrismu_par']);
-  close(gcf),
-  
-  figure, %bar(SAsignorm(:,irel))
+  dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morrismu_par'],options_);
+  if ~options.nodisplay
+    close(hh);
+  end
+
+  hh=dyn_figure(options_); %bar(SAsignorm(:,irel))
 %   boxplot(SAsignorm','whis',10,'symbol','r.')
   myboxplot(SAsignorm',[],'.',[],10)
   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:npT])
@@ -758,10 +754,10 @@ if opt_gsa.morris==1,
   end
   xlabel(' ')
   title('\sigma in the model')
-  saveas(gcf,[OutputDirectoryName,'/',fname_,'_morrissig_par'])
-  eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_morrissig_par']);
-  eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_morrissig_par']);
-  close(gcf),
+  dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morrissig_par'],options_);
+  if ~options.nodisplay
+    close(hh);
+  end
 
   %     figure, bar(SAnorm(:,irel)')
   %     set(gca,'xtick',[1:j0])
@@ -1509,7 +1505,8 @@ else,  % main effects analysis
 %   SAmeanexo=mean(SAmomN(:,1:nshock));
 
 %   figure, bar(latent'*SAcc),
-  figure, bar(sum(SAcc)),
+  hh=dyn_figure(options_);
+  bar(sum(SAcc)),
   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:npT])
   set(gca,'xlim',[0.5 npT+0.5])
   ydum = get(gca,'ylim');
@@ -1521,10 +1518,8 @@ else,  % main effects analysis
   end
   xlabel(' ')
   title(['Identifiability indices in the ',fsuffix,' moments.'],'interpreter','none')
-  saveas(gcf,[OutputDirectoryName,'/',fname_,'_ident_ALL',fsuffix])
-  eval(['print -depsc2 ' OutputDirectoryName '/' fname_ '_ident_ALL',fsuffix]);
-  eval(['print -dpdf ' OutputDirectoryName '/' fname_ '_ident_ALL',fsuffix]);
-
+  dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_ident_ALL',fsuffix],options_);
+  
 %   figure, bar(SAmeanexo),
 %   set(gca,'xticklabel',' ','fontsize',10,'xtick',[1:nshock])
 %   set(gca,'xlim',[0.5 nshock+0.5])

commit 4affab31fd74199399f4bf96e60a148fe5d6f7fc

Fixed bugs introduced in 4c255cf2ae7bd8001ab2fe5290b20c3233cdb6ec

matlab/McMCDiagnostics.m
@@ -194,7 +194,6 @@ if reste
         nc = 3;
     end
     h = dyn_figure(options_,'Name','MCMC univariate diagnostic (Brooks and Gelman, 1998)');
-    end  
     boxplot = 1;
     for j = 1:reste
         k = k+1;

matlab/gsa/filt_mc_.m
@@ -308,7 +308,7 @@ else
                     dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_lnprior',int2str(ifig) ],options_);
                 end
             end
-            if ~options.nodisplay
+            if ~options_.nodisplay
                 close(hh);
             end
         end
@@ -339,7 +339,7 @@ else
                     dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_lnlik',int2str(ifig) ],options_);
                 end
             end
-            if ~options.nodisplay
+            if ~options_.nodisplay
                 close(hh);
             end
         end
@@ -370,7 +370,7 @@ else
                     dyn_saveas(hh,[OutDir '/' fname_ '_rmse_mc_lnpost',int2str(ifig)],options_);
                 end
             end
-            if ~options.nodisplay
+            if ~options_.nodisplay
                 close(hh);
             end
         end

matlab/gsa/map_ident_.m
@@ -96,7 +96,7 @@ if opt_gsa.load_ident_files==0,
       title(M_.exo_names(j,:),'interpreter','none')
       if mod(j,6)==0 | j==M_.exo_nbr,
         dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_vdec_exo_',int2str(ifig)],options_);  
-        if ~options.nodisplay
+        if ~options_.nodisplay
             close(hh);
         end      
       end
@@ -218,7 +218,7 @@ if opt_gsa.morris==1,
   xlabel(' ')
   title('All variance decomposition')
   dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morris_vdec'],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hh);
   end
   else
@@ -737,7 +737,7 @@ if opt_gsa.morris==1,
   xlabel(' ')
   title('\mu in the model')
   dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morrismu_par'],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hh);
   end
 
@@ -755,7 +755,7 @@ if opt_gsa.morris==1,
   xlabel(' ')
   title('\sigma in the model')
   dyn_saveas(hh,[OutputDirectoryName,'/',fname_,'_morrissig_par'],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hh);
   end
 

matlab/gsa/redform_map.m
@@ -157,7 +157,7 @@ for j=1:size(anamendo,1)
         title([logflag,' ',namendo,' vs. ',namexo],'interpreter','none')
         if iplo==9,
           dyn_saveas(hfig,[dirname,'/',M_.fname,'_redform_', namendo,'_vs_shocks_',logflag,num2str(ifig)],options_);  
-          if ~options.nodisplay
+          if ~options_.nodisplay
             close(hfig);
           end
         end
@@ -167,7 +167,7 @@ for j=1:size(anamendo,1)
   end
   if iplo<9 & iplo>0 & ifig,
     dyn_saveas(hfig,[dirname,'/',M_.fname,'_redform_', namendo,'_vs_shocks_',logflag,num2str(ifig)],options_);
-    if ~options.nodisplay
+    if ~options_.nodisplay
         close(hfig);
     end
   end
@@ -233,7 +233,7 @@ for j=1:size(anamendo,1)
         title([logflag,' ',namendo,' vs. ',namlagendo,'(-1)'],'interpreter','none')
         if iplo==9,
           dyn_saveas(hfig,[dirname,'/',M_.fname,'_redform_', namendo,'_vs_lags_',logflag,num2str(ifig)],options_);  
-            if ~options.nodisplay
+            if ~options_.nodisplay
                 close(hfig);
             end
         end
@@ -243,7 +243,7 @@ for j=1:size(anamendo,1)
   end
   if iplo<9 & iplo>0 & ifig,
     dyn_saveas(hfig,[dirname,'/',M_.fname,'_redform_', namendo,'_vs_lags_',logflag,num2str(ifig)],options_);  
-    if ~options.nodisplay
+    if ~options_.nodisplay
       close(hfig);
     end
   end
@@ -301,7 +301,7 @@ if iload==0,
     mkdir(xdir)
   end
   dyn_saveas(hfig,[xdir,'/', namy,'_vs_', namx],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hfig);
   end
 %   gsa_ = gsa_sdp_dyn(y0, x0, -2, [],[],[],1,fname, pnames);
@@ -319,7 +319,7 @@ if iload==0,
   hfig=gsa_sdp_plot(gsa_,fname,pnames,iii(1:min(12,np)));
   close(hfig);
   gsa_.x0=x0(1:nfit,:);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hfig);
   end
 %   copyfile([fname,'_est.mat'],[fname,'.mat'])
@@ -327,7 +327,7 @@ if iload==0,
   plot(y0(1:nfit),[gsa_.fit y0(1:nfit)],'.'), 
   title([namy,' vs. ', namx,' fit'])
   dyn_saveas(hfig,[xdir,'/', namy,'_vs_', namx,'_fit'],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
    close(hfig);
   end
   if nfit<nrun,
@@ -337,7 +337,7 @@ if iload==0,
   plot(y0(npred),[yf y0(npred)],'.'), 
   title([namy,' vs. ', namx,' pred'])
   dyn_saveas(hfig,[xdir,'/', namy,'_vs_', namx,'_pred'],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hfig);
   end
   end
@@ -349,7 +349,7 @@ else
   plot(y0,[yf y0],'.'), 
   title([namy,' vs. ', namx,' pred'])
   dyn_saveas(hfig,[xdir,'/', namy,'_vs_', namx,'_pred'],options_);  
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hfig);
   end
 end

matlab/gsa/redform_screen.m
@@ -91,7 +91,7 @@ for j=1:size(anamendo,1),
         title([namendo,' vs. ',namexo],'interpreter','none')
         if iplo==9,
           dyn_saveas(hh,[dirname,'/',M_.fname,'_', namendo,'_vs_shock_',num2str(ifig)],options_);  
-          if ~options.nodisplay
+          if ~options_.nodisplay
             close(hh);
           end
         end
@@ -101,7 +101,7 @@ for j=1:size(anamendo,1),
   end
   if iplo<9 & iplo>0 & ifig,
     dyn_saveas(hh,[dirname,'/',M_.fname,'_', namendo,'_vs_shocks_',num2str(ifig)],options_);
-    if ~options.nodisplay
+    if ~options_.nodisplay
       close(hh);
     end
   end
@@ -138,7 +138,7 @@ for j=1:size(anamendo,1),
         title([namendo,' vs. ',namlagendo,'(-1)'],'interpreter','none')
         if iplo==9,
           dyn_saveas(hh,[dirname,'/',M_.fname,'_', namendo,'_vs_lags_',num2str(ifig)],options_);  
-          if ~options.nodisplay
+          if ~options_.nodisplay
             close(hh);
           end
         end

matlab/gsa/stab_map_1.m
@@ -98,7 +98,7 @@ for i=1:ceil(nparplot/12),
     title([ftit{j},'. p-value ', num2str(proba(ipar(j)),2)],'interpreter','none')
   end
   dyn_saveas(hh,[dirname,'/',fname_,'_',aname,'_SA_',int2str(i)],options_);
-  if ~options.nodisplay
+  if ~options_.nodisplay
     close(hh);
   end
 end

matlab/gsa/stab_map_2.m
@@ -98,7 +98,7 @@ for j=1:npar,
       title(['cc = ',num2str(c0(i2(jx),j))])
       if (mod(j2,12)==0) && j2>0,
         dyn_saveas(hh,[dirname,'/',fig_nam_,int2str(ifig)],options_);
-        if ~options.nodisplay
+        if ~options_.nodisplay
           close(hh);
         end
       end