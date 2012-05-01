650e67f6b77abf82cb5906879e83d6c401c87191
Michel Juillard <michel.juillard@mjui.fr>
2012-05-01 10:53:23 +0200

    fixing but in previous commit

--- matlab/stochastic_solvers.m
+++ matlab/stochastic_solvers.m
@@ -170,7 +170,7 @@ else
 
     else  % use original Dynare solver
         [dr,info] = dyn_first_order_solver(jacobia_,M_,dr,options_,task);
-        if info || task
+        if info(1) || task
             return;
         end
     end
