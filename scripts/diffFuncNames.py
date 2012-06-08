45988e258a9a2bba5758d317c095f3001545d8e9
Houtan Bastani <houtan.bastani@ens.fr>
2012-06-08 20:01:31 +0200

    add comment/copyright to script

--- scripts/diffFuncNames.py
+++ scripts/diffFuncNames.py
@@ -1,3 +1,29 @@
+##
+## This script recursively goes through the matlab directory and its
+## subdirectories, comparing all .m file names to the function declarations at
+## the top of those files. If the two do not correspond, it prints out the name
+## of the file and the name of the function declared in the file.
+##
+
+##
+## Copyright (C) 2012 Dynare Team
+##
+## This file is part of Dynare.
+##
+## Dynare is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Dynare is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 import os
 import string
 
