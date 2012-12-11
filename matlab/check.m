MichelJuillard/dynare-michel

commit c121aa14b1f9147a13d6b3fee078b8a65ea2fb96

Remove oo_.dr.{nstatic,npred,nboth,nfwrd,nspred,nsfwrd}
Replace them by equivalents in M_ (and an extra one: M_.dynamic).

IMPORTANT POINT: oo_.dr.npred used to count both purely backward and mixed/both
variables. This was the cause of lots of confusion. The new M_.npred only
counts purely backward variables.

We now have the following indentities:

M_.npred + M_.nboth + M_.nfwrd + M_.nstatic = M_.endo_nbr
M_.nspred = M_.npred + M_.nboth
M_.nsfwrd = M_.nfwrd + M_.nboth
M_.ndynamic = M_.npred + M_.nboth + M_.nfwrd

matlab/check.m
@@ -38,7 +38,7 @@ function [result,info] = check(M, options, oo)
 %! @end deftypefn
 %@eod:
 
-% Copyright (C) 2001-2011 Dynare Team
+% Copyright (C) 2001-2012 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -77,16 +77,13 @@ if info(1) ~= 0 && info(1) ~= 3 && info(1) ~= 4
 end
 
 eigenvalues_ = dr.eigval;
-if (options.block)
-    nyf = dr.nyf;
-else
-    nyf = nnz(dr.kstate(:,2)>M.maximum_endo_lag+1);
-end;
 [m_lambda,i]=sort(abs(eigenvalues_));
 n_explod = nnz(abs(eigenvalues_) > options.qz_criterium);
 
+nsfwrd = M.nsfwrd
+
 result = 0;
-if (nyf== n_explod) && (dr.full_rank)
+if (nsfwrd == n_explod) && (dr.full_rank)
     result = 1;
 end
 
@@ -97,7 +94,7 @@ if options.noprint == 0
     z=[m_lambda real(eigenvalues_(i)) imag(eigenvalues_(i))]';
     disp(sprintf('%16.4g %16.4g %16.4g\n',z))
     disp(sprintf('\nThere are %d eigenvalue(s) larger than 1 in modulus ', n_explod));
-    disp(sprintf('for %d forward-looking variable(s)',nyf));
+    disp(sprintf('for %d forward-looking variable(s)',nsfwrd));
     disp(' ')
     if result
         disp('The rank condition is verified.')

doc/dynare.texi
@@ -3387,32 +3387,31 @@ Dynare distinguishes four types of endogenous variables:
 @table @emph
 
 @item Purely backward (or purely predetermined) variables
-@vindex oo_.dr.npred
-@vindex oo_.dr.nboth
+@vindex M_.npred
 Those that appear only at current and past period in the model, but
 not at future period (@i{i.e.} at @math{t} and @math{t-1} but not
 @math{t+1}). The number of such variables is equal to
-@code{oo_.dr.npred - oo_.dr.nboth}.
+@code{M_.npred}.
 
 @item Purely forward variables
-@vindex oo_.dr.nfwrd
+@vindex M_.nfwrd
 Those that appear only at current and future period in the model, but
 not at past period (@i{i.e.} at @math{t} and @math{t+1} but not
 @math{t-1}). The number of such variables is stored in
-@code{oo_.dr.nfwrd}.
+@code{M_.nfwrd}.
 
 @item Mixed variables
-@vindex oo_.dr.nboth
+@vindex M_.nboth
 Those that appear at current, past and future period in the model
 (@i{i.e.} at @math{t}, @math{t+1} and @math{t-1}). The number of such
-variables is stored in @code{oo_.dr.nboth}.
+variables is stored in @code{M_.nboth}.
 
 @item Static variables
-@vindex oo_.dr.nstatic
+@vindex M_.nstatic
 Those that appear only at current, not past and future period in the
 model (@i{i.e.} only at @math{t}, not at @math{t+1} or
 @math{t-1}). The number of such variables is stored in
-@code{oo_.dr.nstatic}.
+@code{M_.nstatic}.
 @end table
 
 Note that all endogenous variables fall into one of these four
@@ -3421,7 +3420,7 @@ categories, since after the creation of auxiliary variables
 and one lag. We therefore have the following identity:
 
 @example
-oo_.dr.npred + oo_.dr.nfwrd + oo_.dr.nstatic = M_.endo_nbr
+M_.npred + M_.both + M_.nfwrd + M_.nstatic = M_.endo_nbr
 @end example
 
 Internally, Dynare uses two orderings of the endogenous variables: the
@@ -3445,11 +3444,14 @@ to the endogenous variable numbered @code{oo_.dr_order_var(k)} in
 declaration order. Conversely, k-th declared variable is numbered
 @code{oo_.dr.inv_order_var(k)} in DR-order.
 
-@vindex oo_.dr.npred
+@vindex M_.nspred
+@vindex M_.nsfwrd
+@vindex M_.ndynamic
 Finally, the state variables of the model are the purely backward variables
 and the mixed variables. They are orderer in DR-order when they appear in
-decision rules elements. There are @code{oo_.dr.npred} such
-variables.
+decision rules elements. There are @code{M_.nspred = M_.npred + M_.nboth} such
+variables. Similarly, one has @code{M_.nfwrd = M_.nfwrd + M_.nboth},
+and @code{M_.ndynamic = M_.nfwrd+M_.nboth+M_.npred}.
 
 @node First order approximation
 @subsection First order approximation
@@ -3542,7 +3544,7 @@ where @math{y^s} is the steady state value of @math{y}, and @math{z_t} is a
 vector consisting of the deviation from the steady state of the state
 variables (in DR-order) at date @math{t-1} followed by the exogenous variables at
 date @math{t} (in declaration order). The vector @math{z_t} is
-therefore of size @math{n_z} = @code{oo_.dr.npred +
+therefore of size @math{n_z} = @code{M_.nspred +
 M_.exo_nbr}.
 
 The coefficients of the decision rules are stored as follows:

matlab/AIM_first_order_solver.m
@@ -82,15 +82,15 @@ function [dr,info]=AIM_first_order_solver(jacobia,M,dr,qz_criterium)
     nd = size(dr.kstate,1);
     nba = nd-sum( abs(dr.eigval) < qz_criterium );
 
-    nyf = dr.nfwrd+dr.nboth;
+    nsfwrd = M.nsfwrd;
 
-    if nba ~= nyf
+    if nba ~= nsfwrd
         temp = sort(abs(dr.eigval));
-        if nba > nyf
-            temp = temp(nd-nba+1:nd-nyf)-1-qz_criterium;
+        if nba > nsfwrd
+            temp = temp(nd-nba+1:nd-nsfwrd)-1-qz_criterium;
             info(1) = 3;
-        elseif nba < nyf;
-            temp = temp(nd-nyf+1:nd-nba)-1-qz_criterium;
+        elseif nba < nsfwrd;
+            temp = temp(nd-nsfwrd+1:nd-nba)-1-qz_criterium;
             info(1) = 4;
         end
         info(2) = temp'*temp;

matlab/UnivariateSpectralDensity.m
@@ -63,13 +63,13 @@ end
 f = zeros(nvar,GridSize);
 ghx = dr.ghx;
 ghu = dr.ghu;
-npred = dr.npred;
-nstatic = dr.nstatic;
+nspred = M_.nspred;
+nstatic = M_.nstatic;
 kstate = dr.kstate;
 order = dr.order_var;
 iv(order) = [1:length(order)];
 nx = size(ghx,2);
-ikx = [nstatic+1:nstatic+npred];
+ikx = [nstatic+1:nstatic+nspred];
 A = zeros(nx,nx);
 k0 = kstate(find(kstate(:,2) <= M_.maximum_lag+1),:);
 i0 = find(k0(:,2) == M_.maximum_lag+1);

matlab/disp_dr.m
@@ -8,7 +8,7 @@ function disp_dr(dr,order,var_list)
 %    var_list [char array]:  list of endogenous variables for which the
 %                            decision rules should be printed
 
-% Copyright (C) 2001-2011 Dynare Team
+% Copyright (C) 2001-2012 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -30,7 +30,7 @@ global M_ options_
 nx =size(dr.ghx,2);
 nu =size(dr.ghu,2);
 if options_.block
-    k = dr.npred + dr.nboth;
+    k = M_.nspred;
     k1 = 1:M_.endo_nbr;
 else
     k = find(dr.kstate(:,2) <= M_.maximum_lag+1);

matlab/disp_model_summary.m
@@ -7,7 +7,7 @@ function disp_model_summary(M,dr)
 %   M         [matlab structure] Definition of the model.           
 %   dr        [matlab structure] Decision rules
 %
-% Copyright (C) 2001-2010 Dynare Team
+% Copyright (C) 2001-2012 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -33,7 +33,7 @@ disp(['  Number of state variables:   ' ...
       int2str(length(find(dr.kstate(:,2) <= M.maximum_lag+1)))])
 disp(['  Number of jumpers:           ' ...
       int2str(length(find(dr.kstate(:,2) == M.maximum_lag+2)))])
-disp(['  Number of static variables:  ' int2str(dr.nstatic)])
+disp(['  Number of static variables:  ' int2str(M.nstatic)])
 my_title='MATRIX OF COVARIANCE OF EXOGENOUS SHOCKS';
 labels = deblank(M.exo_names);
 headers = char('Variables',labels);

matlab/display_conditional_variance_decomposition.m
@@ -16,7 +16,7 @@ function oo_ = display_conditional_variance_decomposition(Steps, SubsetOfVariabl
 % [1] The covariance matrix of the state innovations needs to be diagonal.
 % [2] In this version, absence of measurement errors is assumed...
 
-% Copyright (C) 2010-2011 Dynare Team
+% Copyright (C) 2010-2012 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -40,7 +40,7 @@ StateSpaceModel.number_of_state_innovations = exo_nbr;
 StateSpaceModel.sigma_e_is_diagonal = M_.sigma_e_is_diagonal;
 
 iv = (1:endo_nbr)';
-ic = dr.nstatic+(1:dr.npred)';
+ic = M_.nstatic+(1:M_.nspred)';
 
 [StateSpaceModel.transition_matrix,StateSpaceModel.impulse_matrix] = kalman_transition_matrix(dr,iv,ic,exo_nbr);
 StateSpaceModel.state_innovations_covariance_matrix = M_.Sigma_e;

matlab/dr_block.m
@@ -74,10 +74,6 @@ end;
 mexErrCheck('bytecode', chck);
 dr.full_rank = 1;
 dr.eigval = [];
-dr.nstatic = 0;
-dr.nfwrd = 0;
-dr.npred = 0;
-dr.nboth = 0;
 dr.nd = 0;
 
 dr.ghx = [];
@@ -89,11 +85,6 @@ n_sv = size(dr.state_var, 2);
 dr.ghx = zeros(M_.endo_nbr, length(dr.state_var));
 dr.exo_var = 1:M_.exo_nbr;
 dr.ghu = zeros(M_.endo_nbr, M_.exo_nbr);
-dr.nstatic = M_.nstatic;
-dr.nfwrd = M_.nfwrd;
-dr.npred = M_.npred;
-dr.nboth = M_.nboth;
-dr.nyf = 0;
 for i = 1:Size;
     ghx = [];
     indexi_0 = 0;
@@ -107,7 +98,6 @@ for i = 1:Size;
     n_fwrd = data(i).n_forward;
     n_both = data(i).n_mixed;
     n_static = data(i).n_static;
-    dr.nyf = dr.nyf  + n_fwrd + n_both;
     nd = n_pred + n_fwrd + 2*n_both;
     dr.nd = dr.nd + nd;
     n_dynamic = n_pred + n_fwrd + n_both;

commit 9ec22085b081c5860663ecc340b64b1a3001fdb8

Fix wrong count of forward looking vars in check with block option
Bug introduced in c121aa14b1f9147a13d6b3fee078b8a65ea2fb96

matlab/check.m
@@ -80,10 +80,18 @@ eigenvalues_ = dr.eigval;
 [m_lambda,i]=sort(abs(eigenvalues_));
 n_explod = nnz(abs(eigenvalues_) > options.qz_criterium);
 
-nsfwrd = M.nsfwrd
+% Count number of forward looking variables
+if ~options.block
+    nyf = M.nsfwrd;
+else
+    nyf = 0;
+    for j = 1:length(M.block_structure.block)
+        nyf = nyf + M.block_structure.block(j).n_forward + M.block_structure.block(j).n_mixed;
+    end
+end
 
 result = 0;
-if (nsfwrd == n_explod) && (dr.full_rank)
+if (nyf == n_explod) && (dr.full_rank)
     result = 1;
 end
 
@@ -94,7 +102,7 @@ if options.noprint == 0
     z=[m_lambda real(eigenvalues_(i)) imag(eigenvalues_(i))]';
     disp(sprintf('%16.4g %16.4g %16.4g\n',z))
     disp(sprintf('\nThere are %d eigenvalue(s) larger than 1 in modulus ', n_explod));
-    disp(sprintf('for %d forward-looking variable(s)',nsfwrd));
+    disp(sprintf('for %d forward-looking variable(s)',nyf));
     disp(' ')
     if result
         disp('The rank condition is verified.')