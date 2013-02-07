fac731928480071a3d41a512a7bc9cb1cf49bd5c
Sébastien Villemot <sebastien@dynare.org>
2013-02-07 15:28:43 +0100

    @dynDate.display(): simplify the code by reusing format()

--- matlab/@dynDate/display.m
+++ matlab/@dynDate/display.m
@@ -17,20 +17,5 @@ function display(d)
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
-    fprintf('%s = <dynDate: %d', inputname(1), d.time(1));
-    switch d.freq
-        case 1
-        case 4
-            fprintf('Q')
-        case 12
-            fprintf('M')
-        case 52
-            fprintf('W')
-        otherwise
-            error('Unknown frequency %d', d.freq)
-    end
-    if d.freq ~= 1
-        fprintf('%d', d.time(2))
-    end
-    fprintf('>\n')
+    fprintf('%s = <dynDate: %s>\n', inputname(1), format(d));
 end
