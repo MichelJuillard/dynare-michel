32417def82c12d6cbf8b6109d9f6dc49bedf6927
Johannes Pfeifer <jpfeifer@gmx.de>
2013-04-27 10:58:36 +0200

    Provide stability mapping with more expressive figure titles

--- matlab/gsa/stab_map_1.m
+++ matlab/gsa/stab_map_1.m
@@ -1,4 +1,4 @@
-function [proba, dproba] = stab_map_1(lpmat, ibehaviour, inonbehaviour, aname, iplot, ipar, dirname, pcrit)
+function [proba, dproba] = stab_map_1(lpmat, ibehaviour, inonbehaviour, aname, iplot, ipar, dirname, pcrit, atitle)
 %function [proba, dproba] = stab_map_1(lpmat, ibehaviour, inonbehaviour, aname, iplot, ipar, dirname, pcrit)
 %
 % lpmat =  Monte Carlo matrix
@@ -48,7 +48,10 @@ if nargin<5,
 end
 fname_ = M_.fname;
 if nargin<7,
-  dirname='';;
+  dirname='';
+end
+if nargin<9,
+  atitle=aname;
 end
 
 nshock = estim_params_.nvx;
@@ -82,7 +85,7 @@ if iplot && ~options_.nograph
   ftit=bayestopt_.name(ipar+nshock*(1-ishock));
   
 for i=1:ceil(nparplot/12),
-  hh=dyn_figure(options_,'name',aname);
+  hh=dyn_figure(options_,'name',atitle);
   for j=1+12*(i-1):min(nparplot,12*i),
     subplot(3,4,j-12*(i-1))
     if ~isempty(ibehaviour),
