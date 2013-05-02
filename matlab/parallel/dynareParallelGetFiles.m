MichelJuillard/dynare-michel

commit 27fda91c8c5adcf5b32b6bfb1763ffd3c16e8eaf

Replace relative by absolute filenames
Closes #329

matlab/parallel/dynareParallelGetFiles.m
@@ -48,7 +48,7 @@ for indPC=1:length(Parallel),
             end
             if ischar(NamFileInput0),
                 for j=1:size(NamFileInput0,1),
-                    NamFile(j,:)={['./'],deblank(NamFileInput0(j,:))};
+                    NamFile(j,:)={pwd filesep,deblank(NamFileInput0(j,:))};
                 end
                 NamFileInput = NamFile;
             end

matlab/check_posterior_analysis_data.m
@@ -79,8 +79,8 @@ if isempty(pdfinfo)
 else
     number_of_the_last_post_data_file = length(pdfinfo);
     name_of_the_last_post_data_file = ...
-        [ './' M_.dname ...
-          '/metropolis/' ...
+        [ pwd filesep M_.dname ...
+          filesep 'metropolis' filesep ...
           M_.fname '_' ... 
           generic_post_data_file_name ...
           int2str(number_of_the_last_post_data_file) ...

matlab/independent_metropolis_hastings_core.m
@@ -119,7 +119,7 @@ for b = fblck:nblck,
         set_dynare_seed(options_.DynareRandomStreams.seed+b);
     end
     if (options_.load_mh_file~=0)  && (fline(b)>1) && OpenOldFile(b)
-        load(['./' MhDirectoryName '/' ModelName '_mh' int2str(NewFile(b)) ...
+        load([pwd filesep MhDirectoryName filesep ModelName '_mh' int2str(NewFile(b)) ...
               '_blck' int2str(b) '.mat'])
         x2 = [x2;zeros(InitSizeArray(b)-fline(b)+1,npar)];
         logpo2 = [logpo2;zeros(InitSizeArray(b)-fline(b)+1,1)];

matlab/random_walk_metropolis_hastings_core.m
@@ -142,7 +142,7 @@ for b = fblck:nblck,
         set_dynare_seed(options_.DynareRandomStreams.seed+b);
     end
     if (options_.load_mh_file~=0) && (fline(b)>1) && OpenOldFile(b)
-        load(['./' MhDirectoryName '/' ModelName '_mh' int2str(NewFile(b)) ...
+        load([pwd filesep MhDirectoryName filesep ModelName '_mh' int2str(NewFile(b)) ...
             '_blck' int2str(b) '.mat'])
         x2 = [x2;zeros(InitSizeArray(b)-fline(b)+1,npar)];
         logpo2 = [logpo2;zeros(InitSizeArray(b)-fline(b)+1,1)];

commit 1e0a7289066ddf3591f072f27fed023a66e28e6c

Fix bug introduced in 27fda91c8c5adcf5b32b6bfb1763ffd3c16e8eaf
For parallel computations, the relative path is necessary.

matlab/parallel/dynareParallelGetFiles.m
@@ -48,7 +48,7 @@ for indPC=1:length(Parallel),
             end
             if ischar(NamFileInput0),
                 for j=1:size(NamFileInput0,1),
-                    NamFile(j,:)={pwd filesep,deblank(NamFileInput0(j,:))};
+                    NamFile(j,:)={['./'],deblank(NamFileInput0(j,:))};
                 end
                 NamFileInput = NamFile;
             end