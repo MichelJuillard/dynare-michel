e30cbef21733e90143dda70e17c89b30d877ef2a
Sébastien Villemot <sebastien@dynare.org>
2013-05-17 10:39:05 +0200

    Better fix for the issue raised in cc5527c commit message

--- matlab/k_order_pert.m
+++ matlab/k_order_pert.m
@@ -56,18 +56,6 @@ switch(order)
           info(1)=9;
           return;
         end
-        dr.ghx = derivs.gy;
-        dr.ghu = derivs.gu;
-        dr.ghxx = unfold2(derivs.gyy,nspred);
-        dr.ghxu = derivs.gyu;
-        dr.ghuu = unfold2(derivs.guu,exo_nbr);
-        dr.ghs2 = derivs.gss;
-        dr.ghxxx = unfold3(derivs.gyyy,nspred);
-        dr.ghxxu = unfold21(derivs.gyyu,nspred,exo_nbr);
-        dr.ghxuu = unfold12(derivs.gyuu,nspred,exo_nbr);
-        dr.ghuuu = unfold3(derivs.guuu,exo_nbr);
-        dr.ghxss = derivs.gyss;
-        dr.ghuss = derivs.guss;
     else
         [err, g_0, g_1, g_2, g_3] = k_order_perturbation(dr, ...
                                                          M,options);
@@ -84,57 +72,70 @@ switch(order)
     error('order > 3 isn''t implemented')
 end
 
-if options.pruning 
-    return
-end
+% Now fill in dr.ghx, dr.ghu...
 
-nspred = M.nspred;
+if options.pruning && order == 3
+    dr.ghx = derivs.gy;
+    dr.ghu = derivs.gu;
+    dr.ghxx = unfold2(derivs.gyy,nspred);
+    dr.ghxu = derivs.gyu;
+    dr.ghuu = unfold2(derivs.guu,exo_nbr);
+    dr.ghs2 = derivs.gss;
+    dr.ghxxx = unfold3(derivs.gyyy,nspred);
+    dr.ghxxu = unfold21(derivs.gyyu,nspred,exo_nbr);
+    dr.ghxuu = unfold12(derivs.gyuu,nspred,exo_nbr);
+    dr.ghuuu = unfold3(derivs.guuu,exo_nbr);
+    dr.ghxss = derivs.gyss;
+    dr.ghuss = derivs.guss;
+else
+    nspred = M.nspred;
 
-dr.ghx = dr.g_1(:,1:nspred);
-dr.ghu = dr.g_1(:,nspred+1:end);
+    dr.ghx = dr.g_1(:,1:nspred);
+    dr.ghu = dr.g_1(:,nspred+1:end);
 
-if options.loglinear == 1
-    k = find(dr.kstate(:,2) <= M.maximum_endo_lag+1);
-    klag = dr.kstate(k,[1 2]);
-    k1 = dr.order_var;
+    if options.loglinear == 1
+        k = find(dr.kstate(:,2) <= M.maximum_endo_lag+1);
+        klag = dr.kstate(k,[1 2]);
+        k1 = dr.order_var;
     
-    dr.ghx = repmat(1./dr.ys(k1),1,size(dr.ghx,2)).*dr.ghx.* ...
-             repmat(dr.ys(k1(klag(:,1)))',size(dr.ghx,1),1);
-    dr.ghu = repmat(1./dr.ys(k1),1,size(dr.ghu,2)).*dr.ghu;
-end
+        dr.ghx = repmat(1./dr.ys(k1),1,size(dr.ghx,2)).*dr.ghx.* ...
+                 repmat(dr.ys(k1(klag(:,1)))',size(dr.ghx,1),1);
+        dr.ghu = repmat(1./dr.ys(k1),1,size(dr.ghu,2)).*dr.ghu;
+    end
 
-if order > 1
-    dr.ghs2 = 2*g_0;
-    s0 = 0;
-    s1 = 0;
-    ghxx=zeros(endo_nbr, nspred^2);
-    ghxu=zeros(endo_nbr, nspred*exo_nbr);
-    ghuu=zeros(endo_nbr, exo_nbr^2);
-    for i=1:size(g_2,2)
-        if s0 < nspred && s1 < nspred
-            ghxx(:,s0*nspred+s1+1) = 2*g_2(:,i);
-            if s1 > s0
-                ghxx(:,s1*nspred+s0+1) = 2*g_2(:,i);
+    if order > 1
+        dr.ghs2 = 2*g_0;
+        s0 = 0;
+        s1 = 0;
+        ghxx=zeros(endo_nbr, nspred^2);
+        ghxu=zeros(endo_nbr, nspred*exo_nbr);
+        ghuu=zeros(endo_nbr, exo_nbr^2);
+        for i=1:size(g_2,2)
+            if s0 < nspred && s1 < nspred
+                ghxx(:,s0*nspred+s1+1) = 2*g_2(:,i);
+                if s1 > s0
+                    ghxx(:,s1*nspred+s0+1) = 2*g_2(:,i);
+                end
+            elseif s0 < nspred && s1 < nspred+exo_nbr 
+                ghxu(:,(s0*exo_nbr+s1-nspred+1)) = 2*g_2(:,i);
+            elseif s0 < nspred+exo_nbr && s1 < nspred+exo_nbr
+                ghuu(:,(s0-nspred)*exo_nbr+s1-nspred +1) = 2*g_2(:,i);
+                if s1 > s0
+                    ghuu(:,(s1-nspred)*exo_nbr+s0-nspred+1) = 2*g_2(:,i);
+                end
+            else
+                error('dr1:k_order_perturbation:g_2','Unaccounted columns in g_2');
             end
-        elseif s0 < nspred && s1 < nspred+exo_nbr 
-            ghxu(:,(s0*exo_nbr+s1-nspred+1)) = 2*g_2(:,i);
-        elseif s0 < nspred+exo_nbr && s1 < nspred+exo_nbr
-            ghuu(:,(s0-nspred)*exo_nbr+s1-nspred +1) = 2*g_2(:,i);
-            if s1 > s0
-                ghuu(:,(s1-nspred)*exo_nbr+s0-nspred+1) = 2*g_2(:,i);
+            s1 = s1+1;
+            if s1 == nspred+exo_nbr
+                s0 = s0+1;
+                s1 = s0; 
             end
-        else
-            error('dr1:k_order_perturbation:g_2','Unaccounted columns in g_2');
-        end
-        s1 = s1+1;
-        if s1 == nspred+exo_nbr
-            s0 = s0+1;
-            s1 = s0; 
-        end
-    end % for loop
-    dr.ghxx = ghxx;
-    dr.ghxu = ghxu;
-    dr.ghuu = ghuu;
+        end % for loop
+        dr.ghxx = ghxx;
+        dr.ghxu = ghxu;
+        dr.ghuu = ghuu;
+    end
 end
 
 function y = unfold2(x,n)
