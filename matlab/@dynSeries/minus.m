479034ae6d6d5fe11cca3236d3b81572c56b9bf3
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2013-06-17 17:27:19 +0200

    Partially reverted Ferhat's commit 16a73af066fb52cce1ecc98c5e434d7528db2e1f.

--- matlab/@dynSeries/minus.m
+++ matlab/@dynSeries/minus.m
@@ -38,7 +38,7 @@ function A = minus(B,C)
 %
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
-[B, C] = align(B, C);
+
 if ~isequal(B.vobs,C.vobs) && ~(isequal(B.vobs,1) || isequal(C.vobs,1))
     error(['dynSeries::minus: Cannot substract ' inputname(1) ' and ' inputname(2) ' (wrong number of variables)!'])
 else
