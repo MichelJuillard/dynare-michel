28f3ca1d07053ef634440aec69322acb7700c236
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2013-06-24 12:26:58 +0200

    Added overloaded disp and display methods for dynSeries objects.

---
+++ matlab/@dynSeries/display.m
@@ -0,0 +1,21 @@
+function display(A)
+%@info:
+%! @deftypefn {Function File} display (@var{A})
+%! @anchor{@dynSeries/display}
+%! @sp 1
+%! Overloads the disp method for the Dynare time series class (@ref{dynSeries}).
+%! @sp 2
+%! @strong{Inputs}
+%! @sp 1
+%! @table @ @var
+%! @item A
+%! Dynare time series object instantiated by @ref{dynSeries}.
+%! @end table
+%! @sp 1
+%! @strong{Outputs}
+%! @sp 1
+%! None
+%! @end deftypefn
+%@eod:
+
+disp(A)
