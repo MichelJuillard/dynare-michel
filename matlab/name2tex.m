67ee48cddc40172ce2f56ceb673dc669729e22fd
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2013-07-05 13:34:13 +0200

    Added unitary tests.

--- matlab/name2tex.m
+++ matlab/name2tex.m
@@ -147,3 +147,71 @@ end
 %$
 %$ T = all(t);
 %@eof:1
+
+%@test:2
+%$ t = zeros(16,1);
+%$ t1 = name2tex({'_azert'});
+%$ t2 = name2tex({'azert_'});
+%$ t3 = name2tex({'_azert_'});
+%$ t4 = name2tex({'azert_uiop'});
+%$ t5 = name2tex({'azert_uiop_qsdfg'});
+%$ t6 = name2tex({'azert_uiop_qsdfg_'});
+%$ t7 = name2tex({'_azert_uiop_qsdfg'});
+%$ t8 = name2tex({'_azert_uiop_qsdfg_'});
+%$ t11 = name2tex({'_azert'},1);
+%$ t12 = name2tex({'azert_'},1);
+%$ t13 = name2tex({'_azert_'},1);
+%$ t14 = name2tex({'azert_uiop'},1);
+%$ t15 = name2tex({'azert_uiop_qsdfg'},1);
+%$ t16 = name2tex({'azert_uiop_qsdfg_'},1);
+%$ t17 = name2tex({'_azert_uiop_qsdfg'},1);
+%$ t18 = name2tex({'_azert_uiop_qsdfg_'},1);
+%$
+%$ t(1) = dyn_assert(t1,{'\\_azert'});
+%$ t(2) = dyn_assert(t2,{'azert\\_'});
+%$ t(3) = dyn_assert(t3,{'\\_azert\\_'});
+%$ t(4) = dyn_assert(t4,{'azert\\_uiop'});
+%$ t(5) = dyn_assert(t5,{'azert\\_uiop\\_qsdfg'});
+%$ t(6) = dyn_assert(t6,{'azert\\_uiop\\_qsdfg\\_'});
+%$ t(7) = dyn_assert(t7,{'\\_azert\\_uiop\\_qsdfg'});
+%$ t(8) = dyn_assert(t8,{'\\_azert\\_uiop\\_qsdfg\\_'});
+%$ t(9) = dyn_assert(t11,{'\\_azert'});
+%$ t(10) = dyn_assert(t12,{'azert\\_'});
+%$ t(11) = dyn_assert(t13,{'\\_azert\\_'});
+%$ t(12) = dyn_assert(t14,{'azert_{uiop}'});
+%$ t(13) = dyn_assert(t15,{'azert\\_uiop_{qsdfg}'});
+%$ t(14) = dyn_assert(t16,{'azert\\_uiop_{qsdfg\\_}'});
+%$ t(15) = dyn_assert(t17,{'\\_azert\\_uiop_{qsdfg}'});
+%$ t(16) = dyn_assert(t18,{'\\_azert\\_uiop_{qsdfg\\_}'});
+%$
+%$ T = all(t);
+%@eof:2
+
+%@test:3
+%$ t = zeros(4,1);
+%$ try
+%$     t1 = name2tex({'_azert';'azert_';'_azert_';'azert_uiop';'azert_uiop_qsdfg';'azert_uiop_qsdfg_'});
+%$     t(1) = 1;
+%$ catch
+%$     % Nothing to do here.
+%$ end
+%$
+%$ if t(1)
+%$     try
+%$         t2 = name2tex({'_azert';'azert_';'_azert_';'azert_uiop';'azert_uiop_qsdfg';'azert_uiop_qsdfg_'},1);
+%$         t(2) = 1;
+%$     catch
+%$         % Nothing to do here.
+%$     end
+%$ end
+%$
+%$ if t(1)
+%$     t(3) = dyn_assert(t1,{'\\_azert';'azert\\_';'\\_azert\\_';'azert\\_uiop';'azert\\_uiop\\_qsdfg';'azert\\_uiop\\_qsdfg\\_'});
+%$ end
+%$
+%$ if t(2)
+%$     t(4) = dyn_assert(t2,{'\\_azert';'azert\\_';'\\_azert\\_';'azert_{uiop}';'azert\\_uiop_{qsdfg}';'azert\\_uiop_{qsdfg\\_}'});
+%$ end
+%$
+%$ T = all(t);
+%@eof:3
