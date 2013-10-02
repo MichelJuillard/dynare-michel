fb0ccdd5d26edc41e61581fdd3e1eb572b1ccb00
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2013-10-02 16:45:16 +0200

    Provide more sensible lower and upper bounds for simpsa algorithm.

--- matlab/dynare_estimation_1.m
+++ matlab/dynare_estimation_1.m
@@ -408,7 +408,7 @@ if ~isequal(options_.mode_compute,0) && ~options_.mh_posterior_mode_estimation
         xparam1=BESTEVER.x;
         disp(sprintf('\n Objective function at mode: %f',fval))
       case 10
-        [xparam1, fval, exitflag] = simpsa(func2str(objective_function),xparam1,-inf(size(xparam1)),inf(size(xparam1)),[],dataset_,options_,M_,estim_params_,bayestopt_,oo_);
+        [xparam1, fval, exitflag] = simpsa(func2str(objective_function),xparam1,lb,ub,[],dataset_,options_,M_,estim_params_,bayestopt_,oo_);
       case 11 
          options_.cova_compute = 0 ;
          [xparam1,stdh,lb_95,ub_95,med_param] = online_auxiliary_filter(xparam1,dataset_,options_,M_,estim_params_,bayestopt_,oo_) ;
