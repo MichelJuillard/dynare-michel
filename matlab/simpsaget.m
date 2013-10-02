d9e6985e5b0960742081723bf03513bc06be9aea
Sébastien Villemot <sebastien@dynare.org>
2013-10-02 18:42:36 +0200

    Fix license info for simpsa

--- matlab/simpsaget.m
+++ matlab/simpsaget.m
@@ -16,24 +16,23 @@ function o = SIMPSAGET(options,name,default,flag)
 %   returns val = 1e-4 if the TolX property is not specified in opts.
 %
 %   See also SIMPSASET, SIMPSA
-% 
+
 % Copyright (C) 2006 Brecht Donckels, BIOMATH, brecht.donckels@ugent.be
 % 
-% This program is free software; you can redistribute it and/or
-% modify it under the terms of the GNU General Public License
-% as published by the Free Software Foundation; either version 2
-% of the License, or (at your option) any later version.
-% 
-% This program is distributed in the hope that it will be useful,
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details. 
-% 
+% GNU General Public License for more details.
+%
 % You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
-% USA.
-
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
 
 % undocumented usage for fast access with no error checking
