MichelJuillard/dynare-michel

commit cc0d9b42f0de9be9d2bcda4a763408ba77f9246a

Changed name of options_.maxit as options_.simul.maxit.

mex/sources/bytecode/bytecode.cc
@@ -707,9 +707,9 @@ main(int nrhs, const char *prhs[])
     DYN_MEX_FUNC_ERR_MSG_TXT("verbosity is not a field of options_");
   if (verbose)
     print_it = true;
-  field = mxGetFieldNumber(options_, "maxit_");
+  field = mxGetFieldNumber(options_, "simul.maxit");// Not sure of that...
   if (field < 0)
-    DYN_MEX_FUNC_ERR_MSG_TXT("maxit_ is not a field of options_");
+    DYN_MEX_FUNC_ERR_MSG_TXT("simul.maxit is not a field of options_");
   int maxit_ = int (floor(*(mxGetPr(mxGetFieldByNumber(options_, 0, field)))));
   field = mxGetFieldNumber(options_, "slowc");
   if (field < 0)

matlab/dynare_solve_block_or_bytecode.m
@@ -37,7 +37,7 @@ if options.block && ~options.bytecode
             else
                 n = length(M.block_structure_stat.block(b).variable);
                 [ss, check] = solve_one_boundary([M.fname '_static_' int2str(b)], ss, exo, ...
-                                                 params, [], M.block_structure_stat.block(b).variable, n, 1, 0, b, 0, options.maxit_, ...
+                                                 params, [], M.block_structure_stat.block(b).variable, n, 1, 0, b, 0, options.simul.maxit, ...
                                                  options.solve_tolf, ...
                                                  options.slowc, 0, options.solve_algo, 1, 0, 0,M,options);
                 if check

matlab/ep/extended_path.m
@@ -50,7 +50,7 @@ if isempty(initial_conditions)
 end
 
 % Set maximum number of iterations for the deterministic solver.
-options_.maxit_ = options_.ep.maxit;
+options_.simul.maxit = options_.ep.maxit;
 
 % Set the number of periods for the perfect foresight model
 periods = options_.ep.periods;

matlab/ep/solve_stochastic_perfect_foresight_model.m
@@ -224,7 +224,7 @@ function [flag,endo_simul,err] = solve_stochastic_perfect_foresight_model(endo_s
             fprintf('\n') ;
             disp(['     Total time of simulation        :' num2str(etime(clock,h1))]) ;
             fprintf('\n') ;
-            disp(['WARNING : maximum number of iterations is reached (modify options_.maxit_).']) ;
+            disp(['WARNING : maximum number of iterations is reached (modify options_.simul.maxit).']) ;
             fprintf('\n') ;
         end
         flag = 1;% more iterations are needed.

matlab/ep/solve_stochastic_perfect_foresight_model_1.m
@@ -274,7 +274,7 @@ function [flag,endo_simul,err] = solve_stochastic_perfect_foresight_model_1(endo
             fprintf('\n') ;
             disp(['     Total time of simulation        :' num2str(etime(clock,h1))]) ;
             fprintf('\n') ;
-            disp(['WARNING : maximum number of iterations is reached (modify options_.maxit_).']) ;
+            disp(['WARNING : maximum number of iterations is reached (modify options_.simul.maxit).']) ;
             fprintf('\n') ;
         end
         flag = 1;% more iterations are needed.

matlab/perfect_foresight_simulation.m
@@ -93,14 +93,14 @@ it_init = M_.maximum_lag+1;
 info.convergence = 1;
 info.time  = 0;
 info.error = 0;
-info.iterations.time  = zeros(options_.maxit_,1);
+info.iterations.time  = zeros(options_.simul.maxit,1);
 info.iterations.error = info.iterations.time;
 
-last_line = options_.maxit_;
+last_line = options_.simul.maxit;
 error_growth = 0;
 
 h1 = clock;
-for iter = 1:options_.maxit_
+for iter = 1:options_.simul.maxit
     h2 = clock;
     if options_.terminal_condition
         c = zeros(ny*(periods+1),nrc);

matlab/setup_stochastic_perfect_foresight_model_solver.m
@@ -66,7 +66,7 @@ pfm.i_cols_j = 1:pfm.nd;
 pfm.i_upd = pfm.ny+(1:pfm.periods*pfm.ny);
 pfm.dynamic_model = str2func([DynareModel.fname,'_dynamic']);
 pfm.verbose = DynareOptions.ep.verbosity;
-pfm.maxit_ = DynareOptions.maxit_;
+pfm.maxit_ = DynareOptions.simul.maxit;
 pfm.tolerance = DynareOptions.dynatol.f;
 
 if nargin>3 && DynareOptions.ep.stochastic.order

matlab/sim1.m
@@ -82,7 +82,7 @@ res = zeros(periods*ny,1);
 
     
 h1 = clock ;
-for iter = 1:options_.maxit_
+for iter = 1:options_.simul.maxit
     h2 = clock ;
     
     i_rows = 1:ny;
@@ -156,11 +156,11 @@ elseif ~stop
     skipline();
     fprintf('\nSimulation terminated after %d iterations.\n',iter);
     fprintf('Total time of simulation        : %10.3f\n',etime(clock,h1));
-    fprintf('WARNING : maximum number of iterations is reached (modify options_.maxit_).\n') ;
+    fprintf('WARNING : maximum number of iterations is reached (modify options_.simul.maxit).\n') ;
     oo_.deterministic_simulation.status = 0;% more iterations are needed.
     oo_.deterministic_simulation.error = err;
     %oo_.deterministic_simulation.errors = c/abs(err)    
-    oo_.deterministic_simulation.iterations = options_.maxit_;
+    oo_.deterministic_simulation.iterations = options_.simul.maxit;
 end
 disp (['-----------------------------------------------------']) ;
 skipline();

commit d8052342f386612c5c146ac1ae39d9d58cc100ea

Fixed bug introduced in cc0d9b42f0de9be9d2bcda4a763408ba77f9246a (bytecode.cc).
Problem when reading nested structures.

mex/sources/bytecode/bytecode.cc
@@ -707,10 +707,16 @@ main(int nrhs, const char *prhs[])
     DYN_MEX_FUNC_ERR_MSG_TXT("verbosity is not a field of options_");
   if (verbose)
     print_it = true;
-  field = mxGetFieldNumber(options_, "simul.maxit");// Not sure of that...
-  if (field < 0)
-    DYN_MEX_FUNC_ERR_MSG_TXT("simul.maxit is not a field of options_");
-  int maxit_ = int (floor(*(mxGetPr(mxGetFieldByNumber(options_, 0, field)))));
+  field = mxGetFieldNumber(options_, "simul");
+  mxArray *simul;
+  if (field >= 0)
+    simul = mxGetFieldByNumber(options_, 0, field);
+  else
+    DYN_MEX_FUNC_ERR_MSG_TXT("simul is not a field of options_");
+  field = mxGetFieldNumber(simul, "maxit");
+  if (field<0)
+    DYN_MEX_FUNC_ERR_MSG_TXT("maxit is not a field of options_.simul");
+  int maxit_ = int (floor(*(mxGetPr(mxGetFieldByNumber(simul, 0, field)))));
   field = mxGetFieldNumber(options_, "slowc");
   if (field < 0)
     DYN_MEX_FUNC_ERR_MSG_TXT("slows is not a field of options_");