0264a6b0557da6a01ce9bdc87105d08cf16076e2
Michel Juillard <michel.juillard@mjui.fr>
2013-10-09 13:43:27 +0200

    adding a function dynDates@decimal(dd) that transforms dynDates into a vector of double where the integer part represents the period and the fractional part the subperiod. Maybe we could think of a better name for the function.

---
+++ matlab/@dynDates/dynDates2decimal.m
@@ -0,0 +1,23 @@
+function y = dynDates2decimal(dd)
+% function y = dynDates2decimal(dd)
+%     returns a vector of doubles with the fractional part corresponding
+%     to the subperiod. Mostly used for plots.
+    
+% Copyright (C) 2013 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
+    y = dd.time(:,1)+(dd.time(:,2)-1)/dd.freq;
